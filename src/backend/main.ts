84cada71385461f835b56efae46b5ce260e957d8
Flavio F Lima <flavioislima@gmail.com>
2022-11-11 09:33:03 +0100

    chore: fix imports

--- src/backend/main.ts
+++ src/backend/main.ts
@@ -90,9 +90,7 @@ import {
   iconDark,
   iconLight,
   installed,
-  kofiPage,
   epicLoginUrl,
-  patreonPage,
   sidInfoUrl,
   supportURL,
   tsStore,
@@ -641,8 +639,6 @@ ipcMain.on('openWeblate', async () => openUrlOrFile(weblateUrl))
 ipcMain.on('showAboutWindow', () => showAboutWindow())
 ipcMain.on('openLoginPage', async () => openUrlOrFile(epicLoginUrl))
 ipcMain.on('openDiscordLink', async () => openUrlOrFile(discordLink))
-ipcMain.on('openPatreonPage', async () => openUrlOrFile(patreonPage))
-ipcMain.on('openKofiPage', async () => openUrlOrFile(kofiPage))
 ipcMain.on('openWinePrefixFAQ', async () => openUrlOrFile(wineprefixFAQ))
 ipcMain.on('openWebviewPage', async (event, url) => openUrlOrFile(url))
 ipcMain.on('openWikiLink', async () => openUrlOrFile(wikiLink))
