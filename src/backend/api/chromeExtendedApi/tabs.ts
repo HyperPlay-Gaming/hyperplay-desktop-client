b7c04cb054353710436c9f9d5e46eab83aa71980
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2022-11-22 15:47:29 -0800

    fix tabs update params

--- src/backend/api/chromeExtendedApi/tabs.ts
+++ src/backend/api/chromeExtendedApi/tabs.ts
@@ -6,8 +6,10 @@ export const remove = function (tabIds: number | number[], callback?: any) {
   console.log('called tabs remove')
 }
 
+// tab id is optional in chrome docs but is included in all metamask calls
+// so we make it required
 export const update = function (
-  tabId?: number,
+  tabId: number,
   updateProperties: any,
   callback?: any
 ) {
