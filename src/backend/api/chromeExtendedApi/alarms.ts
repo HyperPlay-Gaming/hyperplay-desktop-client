f6fc60d723d5aa0ecf18b8a09b2c24fa63aa78ee
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2022-11-22 15:56:30 -0800

    disable unused vars and any eslint rules for chrome extended api

--- src/backend/api/chromeExtendedApi/alarms.ts
+++ src/backend/api/chromeExtendedApi/alarms.ts
@@ -9,8 +9,7 @@ export const getAll = function () {
 export const create = function () {
   console.log('alarms create called')
 }
-/* es-lint-disable @typescript-eslint/no-unused-vars */
-// es-lint-disable-next-line @typescript-eslint/no-explicit-any
+
 export const onAlarm = function (onAlarm: (alarm: any) => void) {
   console.log('alarms onAlarm called')
 }
