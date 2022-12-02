49e49f34a5bb95e76c10670d6568a7b1a80df5a7
Ariel Juodziukynas <arieljuod@gmail.com>
2022-12-02 09:26:31 -0300

    [Logs] Add GOGdl and Legendary verbose log for any command output for better troubleshooting (#2119)
    * Add gogdl and legendary verbose log for any command output
    
    * Address CommandMC's feedback

--- src/backend/constants.ts
+++ src/backend/constants.ts
@@ -6,7 +6,7 @@ import { parse } from '@node-steam/vdf'
 
 import { GameConfigVersion, GlobalConfigVersion } from 'common/types'
 import { logDebug, LogPrefix } from './logger/logger'
-import { createNewLogFileAndClearOldOnces } from './logger/logfile'
+import { createNewLogFileAndClearOldOnes } from './logger/logfile'
 import { env } from 'process'
 import { app } from 'electron'
 import { existsSync, readFileSync } from 'graceful-fs'
@@ -55,8 +55,8 @@ const runtimePath = join(toolsPath, 'runtimes')
 const userInfo = join(legendaryConfigPath, 'user.json')
 const imagesCachePath = join(appFolder, 'images-cache')
 
-const { currentLogFile: currentLogFile, lastLogFile: lastLogFile } =
-  createNewLogFileAndClearOldOnces()
+const { currentLogFile, lastLogFile, legendaryLogFile, gogdlLogFile } =
+  createNewLogFileAndClearOldOnes()
 
 const publicDir = resolve(__dirname, '..', app.isPackaged ? '' : '../public')
 const icon = fixAsarPath(join(publicDir, 'app_icon.png'))
@@ -177,6 +177,8 @@ export {
   currentGlobalConfigVersion,
   currentLogFile,
   lastLogFile,
+  legendaryLogFile,
+  gogdlLogFile,
   discordLink,
   execOptions,
   fixAsarPath,
