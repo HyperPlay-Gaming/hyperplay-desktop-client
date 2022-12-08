b27970dbcd58fc7f3dff31583e9a097de44658bf
Paweł Lidwin <62100117+imLinguin@users.noreply.github.com>
2022-12-08 12:11:44 +0100

    fix: pass skip-version-check only for legendary (#2157)

--- src/backend/legendary/games.ts
+++ src/backend/legendary/games.ts
@@ -28,7 +28,8 @@ import {
   configStore,
   gamesConfigPath,
   isLinux,
-  isFlatpak
+  isFlatpak,
+  isCLINoGui
 } from '../constants'
 import { logError, logInfo, LogPrefix } from '../logger/logger'
 import {
@@ -773,6 +774,7 @@ class LegendaryGame extends Game {
       ...offlineFlag,
       ...wineFlag,
       ...shlex.split(launchArguments ?? ''),
+      isCLINoGui ? '--skip-version-check' : '',
       ...shlex.split(gameSettings.launcherArgs ?? '')
     ]
     const wrappers = setupWrappers(
