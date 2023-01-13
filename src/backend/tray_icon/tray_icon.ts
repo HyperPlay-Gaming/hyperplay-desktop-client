Heroic-Games-Launcher/HeroicGamesLauncher

[Refactor] Main window creating and send message to the frontend (#2200)
* Refactor mainWindow reference

* Refactor main window creation and messaging

* small changes

src/backend/tray_icon/tray_icon.ts
@@ -83,7 +83,7 @@ export const contextMenu = (
   const recentsMenu = recentGames.map((game) => {
     return {
       click: function () {
-        handleProtocol(mainWindow, [`heroic://launch/${game.appName}`])
+        handleProtocol([`heroic://launch/${game.appName}`])
       },
       label: game.title
     }
@@ -120,7 +120,7 @@ export const contextMenu = (
     },
     {
       click: function () {
-        handleExit(mainWindow)
+        handleExit()
       },
       label: i18next.t('tray.quit', 'Quit'),
       accelerator: platform === 'darwin' ? 'Cmd+Q' : 'Ctrl+Q'

HyperPlay-Gaming/hyperplay-desktop-client

src/backend/tray_icon/tray_icon.ts
@@ -87,7 +87,7 @@ export const contextMenu = (
   const recentsMenu = recentGames.map((game) => {
     return {
       click: function () {
-        handleProtocol(mainWindow, [`hyperplay://launch/${game.appName}`])
+        handleProtocol([`hyperplay://launch/${game.appName}`])
       },
       label: game.title
     }
@@ -124,7 +124,7 @@ export const contextMenu = (
     },
     {
       click: function () {
-        handleExit(mainWindow)
+        handleExit()
       },
       label: i18next.t('tray.quit', 'Quit'),
       accelerator: platform === 'darwin' ? 'Cmd+Q' : 'Ctrl+Q'