59319722a460322191eed6e47af1c114c4773c67
Flavio F Lima <flavioislima@gmail.com>
2023-01-23 22:43:20 +0100

    fix: remove current download from sidebar

--- src/frontend/components/UI/Sidebar/index.tsx
+++ src/frontend/components/UI/Sidebar/index.tsx
@@ -8,17 +8,14 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 import ContextProvider from 'frontend/state/ContextProvider'
 import onboardingStore from 'frontend/store/OnboardingStore'
-import CurrentDownload from './components/CurrentDownload'
 import SidebarLinks from './components/SidebarLinks'
 import './index.css'
 import Wallet from './components/wallet'
-import { DMQueueElement } from 'common/types'
 import { observer } from 'mobx-react-lite'
 
 const Sidebar = observer(() => {
   const { t } = useTranslation()
   const { sidebarCollapsed, setSideBarCollapsed } = useContext(ContextProvider)
-  const [currentDMElement, setCurrentDMElement] = useState<DMQueueElement>()
   const [badgeText, setBadgeText] = useState('0')
 
   /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
@@ -27,21 +24,10 @@ const Sidebar = observer(() => {
   }
 
   useEffect(() => {
-    window.api.getDMQueueInformation().then(({ elements }) => {
-      setCurrentDMElement(elements[0])
-    })
-
-    const removeHandleDMQueueInformation = window.api.handleDMQueueInformation(
-      (e, elements) => {
-        setCurrentDMElement(elements[0])
-      }
-    )
-
     const removeHandleSetBadgeText =
       window.api.handleSetBadgeTextInRenderer(setBadgeString)
 
     return () => {
-      removeHandleDMQueueInformation()
       removeHandleSetBadgeText()
     }
   }, [])
@@ -49,15 +35,7 @@ const Sidebar = observer(() => {
   return (
     <aside className={classNames('Sidebar', { collapsed: sidebarCollapsed })}>
       <SidebarLinks />
-      <div className="currentDownloads">
-        {currentDMElement && (
-          <CurrentDownload
-            key={currentDMElement.params.appName}
-            appName={currentDMElement.params.appName}
-            runner={currentDMElement.params.runner}
-          />
-        )}
-      </div>
+      <div className="currentDownloads"></div>
 
       <button
         className="Sidebar__item"
