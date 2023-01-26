f18102874fc2c80250f017e88f2870f5842e7148
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-01-25 19:45:42 -0800

    fix home extension id

--- src/frontend/screens/MetaMaskHome/index.tsx
+++ src/frontend/screens/MetaMaskHome/index.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MetaMaskHomeStyles from './index.module.css'
 
 const MetaMaskHome = function () {
+  const [extId, setExtId] = useState('')
+
+  useEffect(() => {
+    window.api.getExtensionId().then((id) => setExtId(id))
+  }, [])
+
   return (
     <>
       <div className={MetaMaskHomeStyles.homeDiv}>
         <webview
           className={MetaMaskHomeStyles.homeWebview}
-          src="chrome-extension://cknhebdapckpgogkjbicdbknhalpcekh/home.html"
+          src={`chrome-extension://${extId}/home.html`}
         />
       </div>
     </>
