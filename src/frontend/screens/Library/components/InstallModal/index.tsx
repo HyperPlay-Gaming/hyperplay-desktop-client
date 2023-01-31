fe15b90d28c2ade6bb9efc417880d662fb450745
Flávio F Lima <flavioislima@gmail.com>
2023-01-31 13:57:32 +0100

    [Fix/Windows] Some small Windows fixes (#2393)
    * fix: do not show platform selection on windows
    
    * feat: show dont ask again button when detecting vcruntime

--- src/frontend/screens/Library/components/InstallModal/index.tsx
+++ src/frontend/screens/Library/components/InstallModal/index.tsx
@@ -131,7 +131,7 @@ export default React.memo(function InstallModal({
   }, [hasWine])
 
   function platformSelection() {
-    const showPlatformSelection = availablePlatforms.length > 1
+    const showPlatformSelection = !isWin && availablePlatforms.length > 1
 
     if (!showPlatformSelection) {
       return null
