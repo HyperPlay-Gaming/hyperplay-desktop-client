9b2c5bebfe26f893199c04dbf9b067f2592261b4
Flávio F Lima <flavioislima@gmail.com>
2023-02-01 16:17:26 +0100

    [Tech] Refactor Download Dialog + Cache GOG Install Info (#2385)
    * feat: add gogInstallInfo store
    
    * feat: add a helper to deal with the platforms parameters on frontend
    
    * feat: simplify platform handler on install modal
    
    * fix: getInstall info not working on gamepage
    
    * feat: cache gog install info
    
    * fix: types
    
    * chore_ log
    
    * fix: reverted some type changes
    
    * chore: logInfo placement
    
    * fix: installPLatform on gamepage
    
    * fix: deadcode
    
    * fix: clear gogInfoStore on clear cache
    
    * fix: gog save reload
    
    * chore: naming (alsto tro trigger tests again)

--- src/backend/gog/games.ts
+++ src/backend/gog/games.ts
@@ -23,8 +23,8 @@ import {
   ExecResult,
   InstallArgs,
   InstalledInfo,
-  InstallPlatform,
-  WineCommandArgs
+  WineCommandArgs,
+  InstallPlatform
 } from 'common/types'
 import { appendFileSync, existsSync, rmSync } from 'graceful-fs'
 import { gamesConfigPath, isWindows, isMac, isLinux } from '../constants'
