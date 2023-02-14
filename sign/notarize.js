90ff7dca8e2294b0c5944f32d840e578a3008059
Flavio F Lima <flavioislima@gmail.com>
2023-02-14 22:33:50 +0100

    feat: add notarization script

---
+++ sign/notarize.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { notarize } = require('electron-notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;  
+  if (electronPlatformName !== 'darwin') {
+    return;
+  }
+
+  const appName = context.packager.appInfo.productFilename;
+
+  return await notarize({
+    tool: 'notarytool',
+    appBundleId: 'gg.hyperplay.HyperPlay',
+    appPath: `${appOutDir}/${appName}.app`,
+    teamId: process.env.TEAMID,
+    appleId: process.env.APPLEID,
+    appleIdPassword: process.env.APPLEIDPASS,
+  });
+};
