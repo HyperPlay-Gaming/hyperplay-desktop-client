01804ece7b60ae66f870d2c04c34163900d4b8cc
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-03-01 02:10:24 -0800

    Update hyperplay_store_preload.ts

--- src/backend/hyperplay_store_preload.ts
+++ src/backend/hyperplay_store_preload.ts
@@ -1,7 +1,8 @@
 import { contextBridge, ipcRenderer } from 'electron'
 
 contextBridge.exposeInMainWorld('api', {
-  getInfo: async () => false,
+  getInfo: async (gameId: string) =>
+    ipcRenderer.invoke('getGameInfo', gameId, 'hyperplay'),
   // returns isInstalled and updateAvailable booleans
   install: async (gameId: string) =>
     ipcRenderer.invoke('addHyperplayGame', gameId),
