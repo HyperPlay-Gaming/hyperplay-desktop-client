e4ac63f8ec639e13b5f1e85d54783080b5c6c0c4
Flavio F Lima <flavioislima@gmail.com>
2023-03-03 10:47:40 +0100

    chore: add notarization logging

--- sign/notarize.js
+++ sign/notarize.js
@@ -4,6 +4,7 @@ const { notarize } = require('@electron/notarize')
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context
   if (electronPlatformName !== 'darwin' || process.env.NOTARIZE === 'false') {
+    console.log('Notarizing skipped')
     return
   }
 
