081202e2b51e48955b71576f9ed18428ee9d4121
Maximiliaan van Dijk <40890064+hellowodl@users.noreply.github.com>
2023-04-04 19:18:30 -0600

    [UI] added alert card to add-sideload-game modal (#239)
    * feat: added alert card to add-sideload-game modal
    
    * point to latest main submodules
    
    * run yarn i18n
    
    ---------
    
    Co-authored-by: BrettCleary <27568879+BrettCleary@users.noreply.github.com>

---
+++ src/frontend/screens/Library/components/InstallModal/SideloadDialog/Warning/index.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { AlertCard, Typography, Images } from '@hyperplay/ui'
+
+import styles from './index.module.scss'
+import { t } from 'i18next'
+
+const { ButtonText } = Typography
+const { WarningIcon } = Images
+
+export default function SideloadDialogWarning() {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className={styles.container}>
+      {isOpen ? (
+        <AlertCard
+          style={{ maxWidth: 670 }}
+          onClose={() => setIsOpen(false)}
+          variant="danger"
+          title={t('sideload.warningTitle', 'Important')}
+          message={t(
+            'sideload.warningMessage',
+            'Side-loading a game is a feature intended for developers and experienced power users. Side-loading an installer that contains malware could compromise your computer and lead to theft of funds.'
+          )}
+        />
+      ) : (
+        <div className={styles.titleContainer} onClick={() => setIsOpen(true)}>
+          <WarningIcon />
+          <ButtonText className={styles.title}>
+            {t('sideload.warningTitle', 'Important')}
+          </ButtonText>
+        </div>
+      )}
+    </div>
+  )
+}
