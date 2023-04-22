065deff3925d5566d157238e7ab276b398e77ab5
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-04-22 01:59:34 -0700

    fix legendary, show play on complete

--- src/frontend/components/UI/DownloadToastManager/index.tsx
+++ src/frontend/components/UI/DownloadToastManager/index.tsx
@@ -6,16 +6,53 @@ import { handleStopInstallation } from 'frontend/helpers/library'
 import ContextProvider from 'frontend/state/ContextProvider'
 import { useTranslation } from 'react-i18next'
 import DownloadToastManagerStyles from './index.module.scss'
+import { launch } from 'frontend/helpers'
+
+const nullProgress: InstallProgress = {
+  bytes: '0',
+  eta: '00:00:00'
+}
 
 export default function DownloadToastManager() {
+  const [latestElement, setLatestElement] = useState<DMQueueElement>()
   const [currentElement, setCurrentElement] = useState<DMQueueElement>()
-  const [progress, setProgress] = useState<InstallProgress>({
-    bytes: '0',
-    eta: '00:00:00'
-  })
+  const [progress, setProgress] = useState<InstallProgress>(nullProgress)
   const [showDownloadToast, setShowDownloadToast] = useState(true)
   const { showDialogModal } = useContext(ContextProvider)
   const { t } = useTranslation('gamepage')
+  const [showPlay, setShowPlay] = useState(false)
+
+  let showPlayTimeout: NodeJS.Timeout | undefined = undefined
+
+  useEffect(() => {
+    // if download queue finishes, show toast in done state with play button for 10 seconds
+    // if the last progress data point is < 99, then it will not show the done state
+    // technically if < 100, we shouldn't show in order to handle the cancelled download case
+    // but legendary sends progress updates infrequently and this gives margin of error for % calc
+    // TODO: receive a reason from download manager as to why the previous download was removed
+    // whether it was cancelled or the download finished
+    if (
+      latestElement === undefined &&
+      progress.percent &&
+      progress.percent > 99
+    ) {
+      setShowPlay(true)
+      // after 10 seconds remove and reset the toast
+      showPlayTimeout = setTimeout(() => {
+        setCurrentElement(undefined)
+        setShowPlay(false)
+      }, 10000)
+      return
+    }
+
+    // handle the case where a download starts within 10s after download queue finishes
+    if (showPlayTimeout) {
+      clearTimeout(showPlayTimeout)
+      showPlayTimeout = undefined
+    }
+    setShowPlay(false)
+    setCurrentElement(latestElement)
+  }, [latestElement, progress])
 
   useEffect(() => {
     window.api.getDMQueueInformation().then(({ elements }: DMQueue) => {
@@ -25,7 +62,7 @@ export default function DownloadToastManager() {
     const removeHandleDMQueueInformation = window.api.handleDMQueueInformation(
       (e: Electron.IpcRendererEvent, elements: DMQueueElement[]) => {
         if (elements) {
-          setCurrentElement(elements[0])
+          setLatestElement(elements[0])
         }
       }
     )
@@ -36,6 +73,7 @@ export default function DownloadToastManager() {
   }, [])
 
   useEffect(() => {
+    setProgress(nullProgress)
     const handleProgressUpdate = async (
       _e: Electron.IpcRendererEvent,
       { appName: appWithProgress, progress: currentProgress }: GameStatus
@@ -85,7 +123,21 @@ export default function DownloadToastManager() {
     )
   }
 
-  const downloadedBytes = parseInt(progress.bytes)
+  let downloadedBytes = parseInt(progress.bytes)
+  if (typeof progress.bytes === 'string') {
+    if (
+      progress.bytes.includes('MB') ||
+      progress.bytes.includes('Mb') ||
+      progress.bytes.includes('mb')
+    )
+      downloadedBytes *= 1048576
+    else if (
+      progress.bytes.includes('GB') ||
+      progress.bytes.includes('Gb') ||
+      progress.bytes.includes('gb')
+    )
+      downloadedBytes *= 1073741824
+  }
   const title = currentElement?.params.gameInfo.title
     ? currentElement?.params.gameInfo.title
     : 'Game'
@@ -95,9 +147,11 @@ export default function DownloadToastManager() {
   const estimatedCompletionTimeInMs = progress.downSpeed
     ? (downloadSizeInBytes / 1048576 / progress.downSpeed) * 1000
     : 0
-  const imgUrl = currentElement?.params.gameInfo.art_cover
+  let imgUrl = currentElement?.params.gameInfo.art_cover
     ? currentElement?.params.gameInfo.art_cover
     : ''
+  if (!imgUrl.includes('http'))
+    imgUrl = currentElement.params.gameInfo.art_square
   const appName = currentElement?.params.gameInfo.app_name
     ? currentElement?.params.gameInfo.app_name
     : ''
@@ -138,7 +192,16 @@ export default function DownloadToastManager() {
           onPauseClick={() => console.log('pause clicked')}
           onStartClick={() => console.log('start clicked')}
           onCloseClick={() => setShowDownloadToast(false)}
-          status="showOnlyCancel"
+          onPlayClick={async () =>
+            launch({
+              appName,
+              t,
+              runner,
+              hasUpdate: false,
+              showDialogModal
+            })
+          }
+          status={showPlay ? 'done' : 'showOnlyCancel'}
         />
       ) : (
         downloadIcon()
