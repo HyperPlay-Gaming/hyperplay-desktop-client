51f5127a92e87d178c24d59e3c3f86a4e2531152
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2023-05-08 15:59:17 -0700

    fix quotes showing on reload

--- src/frontend/components/UI/AppVersion/index.tsx
+++ src/frontend/components/UI/AppVersion/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 
 const storage = window.localStorage
-let lastVersion = storage.getItem('last_version')
+let lastVersion = storage.getItem('last_version')?.replaceAll('"', '')
 if (lastVersion === undefined) {
   lastVersion = ''
 }
