17f14a66e4537025102d7df252a76cda0ab3cc43
Elio Briceño <eliobricenov@gmail.com>
2023-08-13 10:31:09 -0400

    refactor(top-navbar): apply current active tab styles based on the current url in webview navigation store

---
+++ src/frontend/__tests__/helpers/extract-main-domain.test.ts
@@ -0,0 +1,27 @@
+import { extractMainDomain } from 'frontend/helpers/extract-main-domain'
+
+describe('extractMainDomain', () => {
+  it('should correctly extract main domain without subdomain', () => {
+    const url = 'https://epicgames.com/store'
+    const mainDomain = extractMainDomain(url)
+    expect(mainDomain).toEqual('epicgames.com')
+  })
+
+  it('should correctly extract main domain with www subdomain', () => {
+    const url = 'https://www.epicgames.com/store'
+    const mainDomain = extractMainDomain(url)
+    expect(mainDomain).toEqual('epicgames.com')
+  })
+
+  it('should correctly extract main domain with multiple subdomains', () => {
+    const url = 'https://sub.sub.epicgames.com/store'
+    const mainDomain = extractMainDomain(url)
+    expect(mainDomain).toEqual('epicgames.com')
+  })
+
+  it('should return the full domain when no subdomains present', () => {
+    const url = 'https://example.com'
+    const mainDomain = extractMainDomain(url)
+    expect(mainDomain).toEqual('example.com')
+  })
+})
