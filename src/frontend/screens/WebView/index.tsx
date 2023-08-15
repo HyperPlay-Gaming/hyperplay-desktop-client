aab9121e29e5750d630361b5174ca1d82654a06d
Elio Briceño <eliobricenov@gmail.com>
2023-08-15 00:57:11 -0400

    refactor(webview): use old gog regex

--- src/frontend/screens/WebView/index.tsx
+++ src/frontend/screens/WebView/index.tsx
@@ -21,7 +21,6 @@ import { observer } from 'mobx-react-lite'
 import {
   EPIC_LOGIN_URL,
   EPIC_STORE_URL,
-  GOG_EMBED_URL,
   GOG_LOGIN_URL,
   GOG_STORE_URL,
   HYPERPLAY_STORE_URL,
@@ -53,7 +52,7 @@ function WebView() {
     (authStore.authToken !== '' ? '&qamode=' + authStore.authToken : '')
 
   const epicStore = `${EPIC_STORE_URL}/${lang}/`
-  const gogEmbedRegExp = new RegExp(GOG_EMBED_URL)
+  const gogEmbedRegExp = new RegExp('https://embed.gog.com/on_login_success?')
 
   const trueAsStr = 'true' as unknown as boolean | undefined
   const { runner } = useParams() as { runner: Runner }
