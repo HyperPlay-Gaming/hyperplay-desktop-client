7303ae1c2a23352897e5e8c82d03ee505608eb4a
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2023-08-25 16:16:49 -0700

    fix os == mac if and add logs

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -28,16 +28,19 @@ elif [[ "$OSTYPE" == *"darwin"* ]]
 then
     os="mac"
 fi
+echo "OS is $os"
 
 yarn setup
 
 # build
 if [[ "$TEST_PACKAGED" == "true" ]]
 then
-    if [$os == "mac"]
+    if [[ "$os" == "mac"]]
     then
+        echo "Running yarn dist:mac:x64"
         yarn dist:mac:x64
     else
+        echo "Running yarn dist:$os"
         yarn dist:$os
     fi
 else
