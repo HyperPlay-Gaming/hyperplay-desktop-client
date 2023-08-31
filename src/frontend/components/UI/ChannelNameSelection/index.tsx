53cdedd90967a6876d64e5203fb7998dd9fb970f
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2023-08-31 10:17:27 -0700

    fix/main_access_codes

--- src/frontend/components/UI/ChannelNameSelection/index.tsx
+++ src/frontend/components/UI/ChannelNameSelection/index.tsx
@@ -2,26 +2,20 @@ import React from 'react'
 import SelectField from '../SelectField'
 import { GameInfo } from 'common/types'
 import { translateChannelName } from 'frontend/screens/Library/constants'
-import TextInputField from 'frontend/components/UI/TextInputField'
 import { useTranslation } from 'react-i18next'
 
 interface ChannelNameSelectionProps {
   channelNameToInstall: string
   setChannelNameToInstall(name: string): void
   gameInfo: GameInfo
-  accessCode: string
-  setAccessCode(code: string): void
 }
 
 export default function ChannelNameSelection({
   channelNameToInstall,
   setChannelNameToInstall,
   gameInfo,
-  accessCode,
-  setAccessCode
 }: ChannelNameSelectionProps) {
   const { t } = useTranslation('gamepage')
-  const selectedChannel = gameInfo?.channels?.[channelNameToInstall]
   return (
     <>
       <SelectField
@@ -42,14 +36,6 @@ export default function ChannelNameSelection({
             })
           : null}
       </SelectField>
-      {selectedChannel?.license_config.access_codes ? (
-        <TextInputField
-          placeholder={'Enter access code'}
-          value={accessCode}
-          onChange={(ev) => setAccessCode(ev.target.value)}
-          htmlId="access_code_input"
-        ></TextInputField>
-      ) : null}
     </>
   )
 }
