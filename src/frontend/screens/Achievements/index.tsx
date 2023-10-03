765b64e7fc74b582049ef8b40765a2cfb465485e
Andrew James <andrewpjames87@gmail.com>
2023-10-03 14:28:14 -0600

    wip: wire up handle add and put in mock timeout for handling mint

--- src/frontend/screens/Achievements/index.tsx
+++ src/frontend/screens/Achievements/index.tsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import walletStore from 'frontend/store/WalletStore'
 
 import {
   Background,
@@ -10,103 +11,82 @@ import {
 import { Flex, Grid } from '@mantine/core'
 
 import styles from './index.module.css'
-import { StatusIconState } from '@hyperplay/ui/dist/components/AchievementCard/components/StatusIcon'
-import { Game } from '@hyperplay/ui/dist/components/AchievementSummaryTable'
-
-// TODO Remove - Fake Data For Now
-const gamesDefault = [
-  {
-    id: '1',
-    image: 'https://i.imgur.com/Cij5vdL.png',
-    title: 'Diablo II',
-    mintedAchievementsCount: 5,
-    mintableAchievementsCount: 10,
-    state: 'default' as StatusIconState,
-    totalAchievementsCount: 30
-  },
-  {
-    id: '2',
-    image: 'https://i.imgur.com/Cij5vdL.png',
-    title: 'Star Wars: Knights of the Old Republic',
-    mintedAchievementsCount: 5,
-    mintableAchievementsCount: 10,
-    totalAchievementsCount: 30,
-    state: 'default' as StatusIconState,
-    ctaProps: { disabled: true }
-  },
-  {
-    id: '3',
-    image: 'https://i.imgur.com/Cij5vdL.png',
-    title: 'Star Wars: Knights of the Old Republic',
-    mintedAchievementsCount: 5,
-    mintableAchievementsCount: 10,
-    totalAchievementsCount: 30,
-    state: 'active' as StatusIconState
-  },
-  {
-    id: '4',
-    image: '',
-    title: 'Star Wars: Knights of the Old Republic',
-    mintedAchievementsCount: 5,
-    mintableAchievementsCount: 10,
-    totalAchievementsCount: 30,
-    isNewAchievement: true,
-    state: 'default' as StatusIconState
-  }
-]
-
-type AchievementFilter = 'all' | 'new' | 'minted'
+import { Achievement } from 'common/types'
+import { AchievementFilter } from '@hyperplay/ui/dist/components/AchievementSummaryTable'
 
 // TODO: Remove - When api paginates
-function paginate(array: Game[], page: number, perPage: number) {
+function paginate(array: Achievement[], page: number, perPage: number) {
   const start = (page - 1) * perPage;
   const end = start + perPage;
   return array.slice(start, end);
 }
+
 const pageSize = 12
 
 export default React.memo(function Achievements(): JSX.Element {
   const achievementsSortOptions = [{ text: 'Alphabetically' }]
   const [selectedSort, setSelectedSort] = useState(achievementsSortOptions[0])
   const [activeFilter, setActiveFilter] = useState<AchievementFilter>('all')
-  const [achievementsData, setAchievementData] = useState<{ currentPage: number; totalPages: number; games: Game[] }>({ currentPage: 0, totalPages: 0, games: [] })
+  const [achievementsData, setAchievementData] = useState<{ currentPage: number; totalPages: number; games: Achievement[] }>({ currentPage: 0, totalPages: 0, games: [] })
+  const [achievementsToBeMinted, setAchievementsToBeMinted] = useState<string[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
   const filteredGames = useMemo(() => {
     if (activeFilter === 'minted') {
-      return achievementsData.games.filter((game) => game.state === 'active')
+      return achievementsData.games.filter((game) => game.mintedAchievementsCount > 0)
     }
     if (activeFilter === 'new') {
-      return achievementsData.games.filter((game) => game.isNewAchievement)
+      return achievementsData.games.filter((game) => game.mintedAchievementsCount === 0)
     }
     return achievementsData.games
   }, [activeFilter, achievementsData])
 
   useEffect(() => {
     const getAchievements = async () => {
-      const achievementApiData = await window.api.getAchievements('hyperplay')
-      const firstPage = paginate(achievementApiData.data, 1, pageSize) as Game[]
-      const totalPages = Math.ceil(achievementApiData.data.length / pageSize)
-      setAchievementData({ currentPage: 1, totalPages, games: firstPage })
+      const achievementApiData = await window.api.getAchievements('hyperplay') as { data: Achievement[] }
+      const { data } = achievementApiData 
+      const totalPages = Math.ceil(data.length / pageSize)
+      setAchievementData({ currentPage: 1, totalPages, games: paginate(data, 1, pageSize) })
     }
 
     getAchievements()
   }, [])
 
   const handleNextPage = useCallback(async () => {
-    const achievementApiData = await window.api.getAchievements('hyperplay')
-    const totalPages = Math.ceil(achievementApiData.data.length / pageSize)
+    const achievementApiData = await window.api.getAchievements('hyperplay') as { data: Achievement[] }
+    const { data } = achievementApiData 
+    const totalPages = Math.ceil(data.length / pageSize)
     const nextPage = achievementsData.currentPage + 1
-    const data = paginate(achievementApiData.data, nextPage, 12)
-    setAchievementData({ currentPage: nextPage, totalPages, games: data })
+    setAchievementData({ currentPage: nextPage, totalPages, games: paginate(data, nextPage, 12) })
   }, [achievementsData])
 
   const handlePrevPage = useCallback(async () => {
-    const achievementApiData = await window.api.getAchievements('hyperplay')
-    const totalPages = Math.ceil(achievementApiData.data.length / pageSize)
+    const achievementApiData = await window.api.getAchievements('hyperplay') as { data: Achievement[] }
+    const { data } = achievementApiData 
+    const totalPages = Math.ceil(data.length / pageSize)
     const previousPage = achievementsData.currentPage - 1
-    const data = paginate(achievementApiData.data, previousPage, 12)
-    setAchievementData({ currentPage: previousPage, totalPages, games: data })
+    setAchievementData({ currentPage: previousPage, totalPages, games: paginate(data, previousPage, 12) })
   }, [achievementsData])
+
+  const handleAdd = useCallback((id: string) => {
+    if (achievementsToBeMinted.includes(id)) {
+      setAchievementsToBeMinted((state) => state.filter((item) => item !== id))
+    } else {
+      setAchievementsToBeMinted((state) => ([...state, id]))
+    }
+  }, [achievementsToBeMinted])
+
+  const handleMint = useCallback(() => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false)
+      setAchievementsToBeMinted([])
+    }, 3000)
+  }, [achievementsToBeMinted])
+
+  const isDisabled = useMemo(() => {
+    return !walletStore.isConnected || isLoading
+  }, [isLoading, walletStore.isConnected])
 
   return (
     <>
@@ -146,10 +126,29 @@ export default React.memo(function Achievements(): JSX.Element {
           </Grid.Col>
           <Grid.Col
             span={8}
-            className={`${styles.fullHeight} ${styles.achievementTable}`}
+            className={`${styles.fullHeight}`}
           >
             <AchievementSummaryTable
-              games={filteredGames.map(game => ({ id: game.gameId, title: game.gameName, image: game.icon, state: 'disabled', ctaProps: { onClick: () => console.log('click') } }))}
+              games={filteredGames.map((game, index) => {
+                // TODO: Remove - When we have a real achievement id
+                const id = `${game.gameId}-${index}`
+                const state = achievementsToBeMinted.includes(id) ? 'active' : 'default'
+
+                return ({
+                  id: id,
+                  title: game.gameName,
+                  image: game.icon,
+                  mintableAchievementsCount: 40,
+                  mintedAchievementsCount: 20,
+                  totalAchievementsCount: 50,
+                  isNewAchievement: game.mintableAchievementsCount === 0,
+                  state: walletStore.isConnected ? 'disabled' : state,
+                  ctaProps: {
+                    onClick: () => handleAdd(id),
+                    disabled: isDisabled
+                  }
+                })
+              })}
               sortProps={{
                 options: achievementsSortOptions,
                 selected: selectedSort,
@@ -165,7 +164,10 @@ export default React.memo(function Achievements(): JSX.Element {
                 activeFilter,
                 setActiveFilter
               }}
-              mintButtonProps={{ onClick: () => console.log('mint') }}
+              mintButtonProps={{
+                onClick: handleMint,
+                disabled: isDisabled ?? achievementsToBeMinted.length === 0,
+              }}
             />
           </Grid.Col>
         </Grid>
