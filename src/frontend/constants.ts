25ed01b43d26ef6904403920bec6ed730ae670a5
Elio Briceño <eliobricenov@gmail.com>
2023-10-23 09:42:12 -0400

    refactor: update docs site url

--- src/frontend/constants.ts
+++ src/frontend/constants.ts
@@ -2,7 +2,7 @@ export const EPIC_LOGIN_URL = 'https://legendary.gl/epiclogin'
 export const HYPERPLAY_STORE_URL = 'https://store.hyperplay.xyz?isLauncher=true'
 export const EPIC_STORE_URL = 'https://www.epicgames.com/store'
 export const GOG_STORE_URL = `https://gog.com`
-export const WIKI_URL = 'https://docs.hyperplaygaming.com/'
+export const WIKI_URL = 'https://docs.hyperplay.xyz/'
 export const GOG_LOGIN_URL =
   'https://auth.gog.com/auth?client_id=46899977096215655&redirect_uri=https%3A%2F%2Fembed.gog.com%2Fon_login_success%3Forigin%3Dclient&response_type=code&layout=galaxy'
 export const AMAZON_STORE = `https://gaming.amazon.com`
