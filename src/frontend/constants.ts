f6e4a9b365509a408dbfb91f5c2b7350e27af21d
Brett <27568879+BrettCleary@users.noreply.github.com>
2023-10-23 16:51:57 -0500

    feature flag AGS

--- src/frontend/constants.ts
+++ src/frontend/constants.ts
@@ -6,3 +6,4 @@ export const WIKI_URL = 'https://docs.hyperplay.xyz/'
 export const GOG_LOGIN_URL =
   'https://auth.gog.com/auth?client_id=46899977096215655&redirect_uri=https%3A%2F%2Fembed.gog.com%2Fon_login_success%3Forigin%3Dclient&response_type=code&layout=galaxy'
 export const AMAZON_STORE = `https://gaming.amazon.com`
+export const ENABLE_AMAZON_STORE = false
