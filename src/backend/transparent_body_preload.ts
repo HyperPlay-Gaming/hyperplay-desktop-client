1a159691c7512c2cb22f4c4cbc71b0d0f080c1e3
Elio Briceño <eliobricenov@gmail.com>
2023-10-26 21:52:32 -0400

    refactor(auth): update webview dimension and padding

--- src/backend/transparent_body_preload.ts
+++ src/backend/transparent_body_preload.ts
@@ -6,7 +6,7 @@ document.onreadystatechange = function(e)
   if (document.readyState === 'interactive')
   {
     console.log('REMOVING BACKGROUND VIA transparent_body_preload.js')
-    const styles = 'body { background-color: transparent !important; }'
+    const styles = 'body {  } div.root { padding: 0px !important; padding-top: 5px !important; }'
     const styleSheet = document.createElement('style')
     styleSheet.innerText = styles
     document.head.appendChild(styleSheet)
