71f275b0d254f560a11b2545d0b8cf3ff4c4a5b4
Red <red.pace.dev@gmail.com>
2023-11-08 20:29:39 +0100

    feat: Updated private methods to use hash instead

--- src/backend/services/ExtractZipService.ts
+++ src/backend/services/ExtractZipService.ts
@@ -2,6 +2,7 @@ import { EventEmitter } from 'node:events'
 import { Readable } from 'node:stream'
 import { open, ZipFile, Entry } from 'yauzl'
 import { mkdirSync, createWriteStream, rmSync } from 'graceful-fs'
+import { captureException } from '@sentry/electron'
 import { join } from 'path'
 
 export interface ExtractZipProgressResponse {
@@ -20,30 +21,34 @@ export interface ExtractZipProgressResponse {
  * @extends {EventEmitter}
  */
 export class ExtractZipService extends EventEmitter {
-  private readStream: Readable | null = null
-  private canceled = false
-  private paused = false
-  private totalSize = 0
-  private processedSize = 0
-  private startTime = Date.now()
-  private lastUpdateTime = Date.now()
-  private dataDelay = 1000
+  #readStream: Readable | null = null
+  #zipFile = '';
+  #destinationPath = '';
+  #canceled = false
+  #paused = false
+  #totalSize = 0
+  #processedSize = 0
+  #startTime = Date.now()
+  #lastUpdateTime = Date.now()
+  #dataDelay = 1000
 
-  private zipFileInstance: ZipFile | null = null
-  private extractionPromise: Promise<boolean | void> | null = null
-  private resolveExtraction: ((value: boolean) => void) | null = null
-  private rejectExtraction: ((reason: Error | unknown) => void) | null = null
+  #zipFileInstance: ZipFile | null = null
+  #extractionPromise: Promise<boolean | void> | null = null
+  #resolveExtraction: ((value: boolean) => void) | null = null
+  #rejectExtraction: ((reason: Error | unknown) => void) | null = null
 
   /**
    * Creates an instance of ExtractZipService.
    * @param {string} zipFile - The path to the ZIP file.
    * @param {string} destinationPath - The path where the extracted files should be saved.
    */
-  constructor(private zipFile: string, private destinationPath: string) {
+  constructor(zipFile: string, destinationPath: string) {
     super()
 
-    this.resolveExtraction = () => null
-    this.rejectExtraction = () => null
+    this.#zipFile = zipFile;
+    this.#destinationPath = destinationPath;
+    this.#resolveExtraction = () => null
+    this.#rejectExtraction = () => null
   }
 
   /**
@@ -51,7 +56,7 @@ export class ExtractZipService extends EventEmitter {
    * @returns {boolean} - True if the extraction was canceled, false otherwise.
    */
   get isCanceled(): boolean {
-    return this.canceled
+    return this.#canceled
   }
 
   /**
@@ -59,7 +64,7 @@ export class ExtractZipService extends EventEmitter {
    * @returns {boolean} - Current state
    */
   get isPaused(): boolean {
-    return this.paused
+    return this.#paused
   }
 
   /**
@@ -67,7 +72,7 @@ export class ExtractZipService extends EventEmitter {
    * @returns {string} - The path to the ZIP file.
    */
   get source(): string {
-    return this.zipFile
+    return this.#zipFile
   }
 
   /**
@@ -75,15 +80,15 @@ export class ExtractZipService extends EventEmitter {
    * @returns {string} - The destination path.
    */
   get destination(): string {
-    return this.destinationPath
+    return this.#destinationPath
   }
 
   /**
    * Check if can progress
    * @returns {boolean}
    */
-  private get canProgress(): boolean {
-    return !(this.canceled || this.paused)
+  get #canProgress(): boolean {
+    return !(this.#canceled || this.#paused)
   }
 
   /**
@@ -92,11 +97,11 @@ export class ExtractZipService extends EventEmitter {
    */
   public pause(): void {
     if (!this.isPaused) {
-      this.paused = true
+      this.#paused = true
 
-      this.readStream?.pause()
+      this.#readStream?.pause()
 
-      this.emit('paused', this.computeProgress())
+      this.emit('paused', this.#computeProgress())
     }
   }
 
@@ -105,19 +110,19 @@ export class ExtractZipService extends EventEmitter {
    * @returns {Promise<boolean | void>}
    */
   public async resume(): Promise<boolean | void> {
-    if (!this.extractionPromise) {
+    if (!this.#extractionPromise) {
       throw new Error('Extraction has not started or has already completed.')
     }
 
     if (this.isPaused) {
-      this.paused = false
+      this.#paused = false
 
-      this.readStream?.resume()
+      this.#readStream?.resume()
     }
 
-    this.emit('resumed', this.computeProgress())
+    this.emit('resumed', this.#computeProgress())
 
-    return this.extractionPromise
+    return this.#extractionPromise
   }
 
   /**
@@ -125,18 +130,18 @@ export class ExtractZipService extends EventEmitter {
    * @returns {void}
    */
   public cancel() {
-    if (!this.zipFileInstance?.isOpen) {
+    if (!this.#zipFileInstance?.isOpen) {
       throw new Error('Extraction has not started or has already completed.')
     }
 
-    this.canceled = true
+    this.#canceled = true
 
-    this.readStream?.unpipe()
+    this.#readStream?.unpipe()
 
-    this.readStream?.destroy(new Error('Extraction canceled'))
+    this.#readStream?.destroy(new Error('Extraction canceled'))
 
-    if (this.zipFileInstance && this.zipFileInstance.isOpen) {
-      this.zipFileInstance.close()
+    if (this.#zipFileInstance && this.#zipFileInstance.isOpen) {
+      this.#zipFileInstance.close()
     }
 
     this.emit('canceled')
@@ -151,19 +156,19 @@ export class ExtractZipService extends EventEmitter {
    * @private
    * @returns {ExtractZipProgressResponse} - The progress details.
    */
-  private computeProgress(): ExtractZipProgressResponse {
+  #computeProgress(): ExtractZipProgressResponse {
     const progressPercentage = Math.min(
       100,
-      (this.processedSize / this.totalSize) * 100
+      (this.#processedSize / this.#totalSize) * 100
     )
-    const elapsedTime = (Date.now() - this.startTime) / 1000
-    const speed = this.processedSize / elapsedTime
+    const elapsedTime = (Date.now() - this.#startTime) / 1000
+    const speed = this.#processedSize / elapsedTime
 
     return {
       progressPercentage,
       speed,
-      totalSize: this.totalSize,
-      processedSize: this.processedSize
+      totalSize: this.#totalSize,
+      processedSize: this.#processedSize
     }
   }
 
@@ -172,19 +177,19 @@ export class ExtractZipService extends EventEmitter {
    * @private
    * @param {number} chunkLength - The length of the data chunk being processed.
    */
-  private onData(chunkLength: number) {
-    if (!this.canProgress) {
+  #onData(chunkLength: number) {
+    if (!this.#canProgress) {
       return
     }
 
-    this.processedSize += chunkLength
+    this.#processedSize += chunkLength
     const currentTime = Date.now()
 
     // Make always sure to have a delay, unless it will be too much spam + performance issues eventually,
     // especially on electron with webContents.send*
-    if (currentTime - this.lastUpdateTime > this.dataDelay) {
-      this.emit('progress', this.computeProgress())
-      this.lastUpdateTime = currentTime
+    if (currentTime - this.#lastUpdateTime > this.#dataDelay) {
+      this.emit('progress', this.#computeProgress())
+      this.#lastUpdateTime = currentTime
     }
   }
 
@@ -192,12 +197,12 @@ export class ExtractZipService extends EventEmitter {
    * Handles end events after extraction completes.
    * @private
    */
-  private onEnd() {
+  #onEnd() {
     if (this.isCanceled) {
       return
     }
 
-    this.emit('finished', this.computeProgress())
+    this.emit('finished', this.#computeProgress())
 
     rmSync(this.source, { recursive: true, force: true })
 
@@ -209,7 +214,7 @@ export class ExtractZipService extends EventEmitter {
    * @private
    * @param {Error} error - The error that occurred.
    */
-  private onError(error: Error) {
+  #onError(error: Error) {
     this.emit('error', error)
 
     rmSync(this.source, { recursive: true, force: true })
@@ -229,7 +234,7 @@ export class ExtractZipService extends EventEmitter {
       open(
         zipFile,
         { lazyEntries: true, autoClose: true },
-        (err, file: ZipFile) => {
+        (err: unknown, file: ZipFile) => {
           if (err) {
             reject(err)
             return
@@ -249,7 +254,7 @@ export class ExtractZipService extends EventEmitter {
           })
 
           file.on('error', (err: Error) => {
-            this.onError(err)
+            this.#onError(err)
             resolve(0)
           })
         }
@@ -262,93 +267,94 @@ export class ExtractZipService extends EventEmitter {
    * @returns {Promise<boolean | void>} - A promise that resolves when the extraction is complete.
    */
   async extract() {
-    this.extractionPromise = new Promise<boolean | void>((resolve, reject) => {
-      this.resolveExtraction = resolve
-      this.rejectExtraction = reject
+    this.#extractionPromise = new Promise<boolean>((resolve, reject) => {
+      this.#resolveExtraction = resolve
+      this.#rejectExtraction = reject
 
       open(
-        this.zipFile,
+        this.#zipFile,
         { lazyEntries: true, autoClose: true },
-        (err, file: ZipFile) => {
+        (err: unknown, file: ZipFile) => {
           if (err) {
-            this.rejectExtraction?.(err)
+            this.#rejectExtraction?.(err)
             return
           }
 
-          this.zipFileInstance = file
+          this.#zipFileInstance = file
 
-          this.zipFileInstance.readEntry()
+          this.#zipFileInstance.readEntry()
 
-          this.zipFileInstance.on('entry', (entry: Entry) => {
+          this.#zipFileInstance.on('entry', (entry: Entry) => {
             if (this.isCanceled) {
-              this.zipFileInstance?.close()
+              this.#zipFileInstance?.close()
               return
             }
 
             if (/\/$/.test(entry.fileName)) {
               // Directory file names end with '/'
-              mkdirSync(join(this.destinationPath, entry.fileName), {
+              mkdirSync(join(this.#destinationPath, entry.fileName), {
                 recursive: true
               })
-              this.zipFileInstance?.readEntry()
+              this.#zipFileInstance?.readEntry()
             } else {
               // Ensure parent directory exists
               mkdirSync(
                 join(
-                  this.destinationPath,
+                  this.#destinationPath,
                   entry.fileName.split('/').slice(0, -1).join('/')
                 ),
                 { recursive: true }
               )
 
-              this.zipFileInstance?.openReadStream(entry, (err, readStream) => {
-                if (err && this.rejectExtraction) {
-                  this.rejectExtraction(err)
+              this.#zipFileInstance?.openReadStream(entry, (err: unknown, readStream: Readable) => {
+                if (err && this.#rejectExtraction) {
+                  this.#rejectExtraction(err)
                   return
                 }
 
-                this.readStream = readStream
+                this.#readStream = readStream
                 const writeStream = createWriteStream(
-                  join(this.destinationPath, entry.fileName)
+                  join(this.#destinationPath, entry.fileName)
                 )
-                this.readStream.pipe(writeStream)
-                this.readStream.on('data', (chunk) => {
-                  this.onData(chunk.length)
+                this.#readStream.pipe(writeStream)
+                this.#readStream.on('data', (chunk: unknown[]) => {
+                  this.#onData(chunk.length)
                 })
                 writeStream.once('close', () => {
                   if (this.isCanceled) {
                     return
                   }
-                  this.zipFileInstance?.readEntry()
+                  this.#zipFileInstance?.readEntry()
                 })
               })
             }
           })
 
-          this.zipFileInstance.once('end', () => {
+          this.#zipFileInstance.once('end', () => {
             if (this.isCanceled) {
               return
             }
 
-            this.onEnd()
+            this.#onEnd()
 
-            this.resolveExtraction?.(true)
+            this.#resolveExtraction?.(true)
           })
 
-          this.zipFileInstance.once('error', this.onError.bind(this))
+          this.#zipFileInstance.once('error', this.#onError.bind(this))
         }
       )
-    }).catch(this.onError.bind(this))
+    }).catch(this.#onError.bind(this))
     try {
-      this.totalSize = await this.getUncompressedSize(this.zipFile)
-      return await this.extractionPromise
+      this.#totalSize = await this.getUncompressedSize(this.#zipFile)
+      return await this.#extractionPromise
     } catch (error) {
-      this.rejectExtraction?.(error)
+      this.#rejectExtraction?.(error)
+      captureException(error);
     } finally {
-      this.zipFileInstance = null
-      this.extractionPromise = null
-      this.resolveExtraction = null
-      this.rejectExtraction = null
+      this.#zipFileInstance = null
+      this.#extractionPromise = null
+      this.#resolveExtraction = null
+      this.#rejectExtraction = null
     }
   }
 }
