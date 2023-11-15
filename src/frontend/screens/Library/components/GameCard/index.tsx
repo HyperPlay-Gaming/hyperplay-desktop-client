838b701175a20723eb13fe82f86a5b87813285e5
Flavio F Lima <flavioislima@users.noreply.github.com>
2023-11-15 10:09:31 +0000

    fix: update from gamecard

--- src/frontend/screens/Library/components/GameCard/index.tsx
+++ src/frontend/screens/Library/components/GameCard/index.tsx
@@ -43,7 +43,9 @@ const GameCard = ({
   favorited,
   gameInfo: gameInfoFromProps
 }: Card) => {
-  const [gameInfo, setGameInfo] = useState<GameInfo>(gameInfoFromProps)
+  const [gameInfo, setGameInfo] = useState<GameInfo>(
+    JSON.parse(JSON.stringify(gameInfoFromProps))
+  )
   const [showUninstallModal, setShowUninstallModal] = useState(false)
   const [isLaunching, setIsLaunching] = useState(false)
   const [showStopInstallModal, setShowStopInstallModal] = useState(false)
@@ -86,11 +88,6 @@ const GameCard = ({
     }
     updateGameInfo()
   }, [status])
-
-  async function handleUpdate() {
-    if (gameInfo.runner !== 'sideload')
-      updateGame({ appName, runner, gameInfo })
-  }
 
   const {
     isInstalling,
@@ -206,7 +203,7 @@ const GameCard = ({
     {
       // update
       label: t('button.update', 'Update'),
-      onClick: handleClickStopBubbling(async () => handleUpdate()),
+      onClick: handleClickStopBubbling(async () => updateGame(gameInfo)),
       show: hasUpdate && !isUpdating && !isQueued
     },
     {
@@ -225,9 +222,8 @@ const GameCard = ({
       // open the game page
       label: t('button.details', 'Details'),
       onClick: handleClickStopBubbling(() => {
-        const gameInfoDeepClone = JSON.parse(JSON.stringify(gameInfo))
         navigate(`/gamepage/${runner}/${appName}`, {
-          state: { gameInfo: gameInfoDeepClone }
+          state: { gameInfo }
         })
       }),
       show: true
@@ -316,7 +312,7 @@ const GameCard = ({
       )}
       <Link
         to={`/gamepage/${runner}/${appName}`}
-        state={{ gameInfo: JSON.parse(JSON.stringify(gameInfo)) }}
+        state={{ gameInfo }}
         className={classNames({
           gamepad: activeController
         })}
@@ -362,7 +358,9 @@ const GameCard = ({
             () => setShowSettings(!showSettings),
             true
           )}
-          onUpdateClick={handleClickStopBubbling(async () => handleUpdate())}
+          onUpdateClick={handleClickStopBubbling(async () =>
+            updateGame(gameInfo)
+          )}
           progress={progress}
           message={getMessage()}
           actionDisabled={isLaunching}
