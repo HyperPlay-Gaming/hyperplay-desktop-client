08e97b40ffef8381bad8cce0cbc7382ceb29112d
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2023-12-06 21:25:43 -0400

    [TECH]: auth integration part 1 (#563)
    * refactor(auth): add auth session and preload scripts
    
    * refactor(auth): add auth modal component
    
    * refactor(auth): show extension modal when there are already requests
    
    * refactor(auth): show onboarding modal if no provider is installed
    
    * refactor(auth): update explanation
    
    * refactor(auth): remove log
    
    * refactor(auth): handle account connected
    
    * refactor(auth): update yarn lock
    
    * refactor(auth): guard modal by qa mode
    
    * refactor(auth): add alert
    
    * refactor(auth): restore preview url
    
    * refactor(auth): rename state class name and use private fields
    
    * refactor(auth): use getConnectedProvider instead of getCurrentWeb3Provider
    
    * refactor(providers): make return type of getConnectedProvider enum
    
    * refactor(providers): make return type of getConnectedProvider enum
    
    * refactor(auth-state): use private modifier instead of private fields
    
    * refactor(auth): open modal when qa mode is activated
    
    * refactor(auth): track pending signature requests and resume after wallet is connected
    
    * refactor(proxy-server): update commit
    
    * handle destroyed wc sender
    
    * fix mm reload after import
    
    * update mm
    
    * reopen auth modal on app reload after import (#607)
    
    * refactor(auth): update webview dimension and padding
    
    * feat(auth): support email verified
    
    * fix(auth): add back transparent background
    
    * test: add prependListener mock
    
    * chore: prettier fix
    
    * add manage hp accounts dropdown option
    
    * prevent auth modal reopen on every launch
    
    * rm launch auth modal on qa mode, rm alert
    
    * fix imports
    
    * revert mm changes
    
    * fix onboarding on first app load
    
    * fix email verify stuck
    
    * rm log
    
    * mv authStore to authState
    
    * close sign in modal on email confirm
    
    * change account dropdown zIndex to be > mm ext
    
    * add email-confirmation protocol handler
    
    * update email confirmation protocol
    
    * prettier
    
    * add try catch to reload
    
    * rm debug logs
    
    * fix: Fixed submodules not updated, which fails pipelines
    
    * point to submodule auth branches
    
    * add test, fix mac deeplink
    
    * chore(auth): use extension api to show popup instead of state
    
    * chore(auth): use extension api to show popup instead of state
    
    * chore: run lint
    
    * feat(auth): add is launcher query parameter
    
    * refactor(auth): remove unused import
    
    * refactor(auth): use fix branch for provider helper
    
    * refactor(auth): use fix branch for provider helper
    
    * refactor(auth): use main branch in submodules
    
    ---------
    
    Co-authored-by: Brett <27568879+BrettCleary@users.noreply.github.com>
    Co-authored-by: Red <red.pace.dev@gmail.com>

---
+++ src/frontend/components/UI/AuthModal/index.tsx
@@ -0,0 +1,160 @@
+import React, { useEffect, useRef } from 'react'
+import styles from './index.module.scss'
+import { ModalAnimation } from '@hyperplay/ui'
+import { WebviewTag } from 'electron'
+import { observer } from 'mobx-react-lite'
+import authState from '../../../state/authState'
+import { ethers } from 'ethers'
+import extensionState from '../../../state/ExtensionState'
+import onboardingState from '../../../store/OnboardingStore'
+import walletState from '../../../state/WalletState'
+import { DEV_PORTAL_URL } from 'common/constants'
+
+const url = `${DEV_PORTAL_URL}/signin?isLauncher=true`
+
+const METAMASK_ALREADY_PROVIDED_ERROR_CODE = -32002
+
+const isTooManyRequestsError = (error: string) => {
+  return error.includes(METAMASK_ALREADY_PROVIDED_ERROR_CODE.toString())
+}
+
+const AuthModal = () => {
+  const webviewRef = useRef<WebviewTag>(null)
+
+  const sendRetryConnectionMessage = () => {
+    const webview = webviewRef.current
+    if (!webview) return
+    authState.setPendingSignatureRequest(false)
+    webview.send('auth:retryWalletConnection')
+  }
+
+  const handleAccountNotConnected = async () => {
+    const currentProvider = await window.api.getConnectedProvider()
+
+    if (currentProvider === 'Unconnected') {
+      window.api.openAuthModalIfAppReloads()
+      onboardingState.openOnboarding()
+      authState.setPendingSignatureRequest(true)
+      return
+    }
+
+    // try to trigger metamask popup to connect account
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum)
+      await provider.getSigner()
+    } catch (e) {
+      // if it fails, open the popup manually
+      // since there are already requests in the queue, this will resume
+      // the connection flow after the user unlocks metamask
+      if (isTooManyRequestsError(String(e))) {
+        authState.setPendingSignatureRequest(true)
+        extensionState.showPopup()
+      }
+    }
+  }
+
+  useEffect(() => {
+    const webview = webviewRef.current
+    if (!webview) return
+
+    const handleIpcMessage = async (event: Electron.IpcMessageEvent) => {
+      switch (event.channel) {
+        case 'closeAuthModal':
+          authState.closeSignInModal()
+          break
+        case 'auth:accountConnected':
+          authState.setSignedIn()
+          authState.closeSignInModal()
+          break
+        case 'auth:accountNotConnected':
+          await handleAccountNotConnected()
+          break
+        default:
+          break
+      }
+    }
+
+    const handleDomReady = () => {
+      webview.addEventListener('ipc-message', handleIpcMessage)
+    }
+
+    webview.addEventListener('dom-ready', handleDomReady)
+
+    const qaModeListenerCleanup = window.api.handleQaModeActivated(() => {
+      authState.activateQaMode()
+    })
+
+    const oAuthCompletedCleanup = window.api.handleOAuthCompleted(() => {
+      authState.setSignedIn()
+      authState.closeSignInModal()
+    })
+
+    return () => {
+      qaModeListenerCleanup()
+      oAuthCompletedCleanup()
+      webview.removeEventListener('dom-ready', handleDomReady)
+      webview.removeEventListener('ipc-message', handleIpcMessage)
+    }
+  }, [])
+
+  /**
+   * Without reload, user gets stuck on email verified page even after
+   * auth modal close and reopen.
+   */
+  useEffect(() => {
+    if (!authState.isSignInModalOpen) {
+      return
+    }
+    /**
+     * On import app reload this will fail as it tries to reload an unmounted webview
+     */
+    try {
+      webviewRef.current?.reload()
+    } catch (err) {
+      console.error(err)
+    }
+  }, [authState.isSignInModalOpen])
+
+  useEffect(() => {
+    if (walletState.isConnected && authState.hasPendingSignatureRequest) {
+      sendRetryConnectionMessage()
+    }
+  }, [walletState.isConnected, authState.hasPendingSignatureRequest])
+
+  function emailConfirmed(
+    _e: Electron.IpcRendererEvent,
+    emailConfirmUrl: string
+  ) {
+    webviewRef.current?.loadURL(emailConfirmUrl)
+    authState.openSignInModal()
+
+    setTimeout(() => webviewRef.current?.loadURL(url), 5000)
+  }
+
+  useEffect(() => {
+    const rmHandleEmailConfirmationNavigation =
+      window.api.handleEmailConfirmationNavigation(emailConfirmed)
+
+    return () => {
+      rmHandleEmailConfirmationNavigation()
+    }
+  }, [])
+
+  return (
+    <ModalAnimation
+      isOpen={authState.isSignInModalOpen}
+      onClose={() => authState.closeSignInModal()}
+    >
+      <webview
+        ref={webviewRef}
+        src={url}
+        className={styles.webview}
+        partition="persist:auth"
+        allowpopups={'true' as unknown as boolean | undefined}
+        useragent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15"
+      />
+    </ModalAnimation>
+  )
+}
+
+export default observer(AuthModal)
---
+++ src/frontend/state/authState.ts
@@ -0,0 +1,59 @@
+import { onboardingStore } from 'frontend/helpers/electronStores'
+import { makeAutoObservable } from 'mobx'
+
+class AuthState {
+  private qaModeActive = false
+  private signInModalOpen = false
+  private signedIn = false
+  private pendingSignatureRequest = false
+  authToken = ''
+
+  constructor() {
+    makeAutoObservable(this)
+    this.signInModalOpen = onboardingStore.get(
+      'openAuthModalIfAppReloads',
+      false
+    )
+    onboardingStore.set('openAuthModalIfAppReloads', false)
+  }
+
+  get hasPendingSignatureRequest() {
+    return this.pendingSignatureRequest
+  }
+
+  get isSignInModalOpen() {
+    return this.signInModalOpen
+  }
+
+  get isSignedIn() {
+    return this.signedIn
+  }
+
+  get isQaModeActive() {
+    return this.qaModeActive
+  }
+
+  openSignInModal() {
+    this.signInModalOpen = true
+  }
+
+  closeSignInModal() {
+    this.signInModalOpen = false
+  }
+
+  activateQaMode() {
+    this.qaModeActive = true
+  }
+
+  setSignedIn() {
+    this.signedIn = true
+  }
+
+  setPendingSignatureRequest(pending: boolean) {
+    this.pendingSignatureRequest = pending
+  }
+}
+
+const authState = new AuthState()
+
+export default authState
