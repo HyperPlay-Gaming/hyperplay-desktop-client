39568f32bb3251c5c1f150e8b1a16be99307fece
Brett <27568879+BrettCleary@users.noreply.github.com>
2023-12-14 14:48:43 -0800

    mv util fxn

--- src/frontend/helpers/achievementUtils.ts
+++
@@ -1,6 +0,0 @@
-export function isTimestampInPast(unixTimestamp: number) {
-  const currentTime = new Date().getTime()
-  const timestampInMilliseconds = unixTimestamp * 1000 // Convert to milliseconds
-
-  return timestampInMilliseconds < currentTime
-}
