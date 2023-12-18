188b2ace8e8ba0e5088a9eb94874295f8f5f96a3
Andrew James <andrewpjames87@gmail.com>
2023-12-18 16:59:04 -0700

    Feat/achievements (#606)
    * add getAchievements with mock json
    
    * prettier
    
    * WIP: rought in front end components
    
    * chore: prettier
    
    * chore: yarn lock
    
    * wip: add type to mocked data
    
    * wip: consume mock data
    
    * wip: wire up handle add and put in mock timeout for handling mint
    
    * add props to summary, add individual achievement api method
    
    * chore: render test data until api returns correct types
    
    * chore: dont use real data just yet
    
    * feat: add pagination, fliters and sort
    
    * chore: prettier
    
    * feat: update achievements submodule
    
    * feat: update achievements api
    
    * feat: allow filters and sort to make api call
    
    * chore: pull achievement layout out into parent route
    
    * wip: add game achievements page
    
    * feat: add pagination and sorting to getIndividualAchievemnts
    
    * feat: add data for achievement stats
    
    * chore: prettier
    
    * feat: allow mint selection to persist across achievement pages
    
    * fix: prettir
    
    * fix: lint
    
    * chore: update sub module branch
    
    * chore: remove getGame function
    
    * chore: move freeMints into stats
    
    * chore: prettier
    
    * chore: update to numFreeMints
    
    * feat: handle mint and update with separate buttons
    
    * chore: bump hyperplay ui
    
    * fix: put hyperplay extension helper back to main commit
    
    * fix: prettier
    
    * feat: add updateButtonProps to game achievement
    
    * feat: bump ui package 0.1.24
    
    * feat: add accounts tab and steam id input to it
    
    * add flag (#594)
    
    * wip: use real end points to get data
    
    * feat: update backend commit
    
    * fix: disabled buttons and update state
    
    * chore: update api commit
    
    * chore: remove log
    
    * feat: remove mock data
    
    * chore: update api commit
    
    * feat: hide achievements nav behind feature flag
    
    * chore: clean up code
    
    * feat: show stores active and disabled
    
    * feat: allow user to click and change selected store
    
    * feat: use real data for stats
    
    * chore: share stats through store provider
    
    * chore: only sync in store
    
    * chore: update api commit
    
    * chore: update types
    
    * feat: update store row to have refresh button
    
    * fix: prettier
    
    * chore: refactor achievment contexts into mobx classes
    
    * chore: pull getSummaryAchievements into state
    
    * chore: pull out getIndividualAchievements into mobx class
    
    * fix: prettier
    
    * chore: move showAchievements into mobx class
    
    * chore: update to hyperplay ui 0.1.32
    
    * feat: refetch stats and achievements when wallet connects
    
    * update ext helper to main
    
    * fix translations
    
    * fix merge errors
    
    * prettier
    
    * rename achievementstorestate
    
    * fix layout and increase page size
    
    * back button individual game achievements page
    
    * fix height no achievements
    
    * add infinite scroll and fetch for summaries
    
    * add refresh animation
    
    * add store key, fix individual achievements page fetching
    
    * disable eye for single store
    
    * update for latest ui loading changes
    
    * hide minted tab if none minted
    
    * add syncing ui indicator
    
    * add no achievements message
    
    * update props for new achievements ui components
    
    * preload images
    
    * update mint achievement state
    
    * replace all in setter
    
    * move timestamp check to helpers
    
    * use deconstructor to init
    
    * move sort updated fxn up
    
    * use deconstructor for achievement stores
    
    * fix spelling of achievement stores
    
    * move sync handler
    
    * use obj deconstructor
    
    * revert loading bg color
    
    * add hp ui
    
    * fix ui merge issues
    
    * update submodules
    
    * update translations
    
    * prettier
    
    * rm on close
    
    * mv util fxn
    
    * udpate import
    
    * handle refresh
    
    * add console error
    
    * refactor currentSummaryKey getter
    
    * refactor state if else
    
    * add free mints tranlsation key
    
    * clear cache on new steam id
    
    * share achievement nav prop translations summary and details pages
    
    * prettier
    
    ---------
    
    Co-authored-by: Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
    Co-authored-by: Bobby Blackstock <bobbyblackstock@gmail.com>

---
+++ src/frontend/state/AchievementState.ts
@@ -0,0 +1,319 @@
+import { makeAutoObservable, runInAction } from 'mobx'
+import walletState from 'frontend/state/WalletState'
+import type {
+  Achievement,
+  AchievementFilter,
+  AchievementSort,
+  AchievementStore,
+  AchievementsStats,
+  SummaryAchievement
+} from 'common/types'
+
+interface AchievementData {
+  data: SummaryAchievement[]
+  pagesFetched: number
+  totalPages: number
+}
+
+interface AchievementRequestKey {
+  filter: AchievementFilter
+  sort: AchievementSort
+  store: AchievementStore
+}
+
+interface AchievementSortOption {
+  text: string
+  value: AchievementSort
+}
+
+interface IndividualAchievementKey {
+  gameId: string
+  sort: AchievementSort
+  store: AchievementStore
+}
+
+interface IndividualAchievementData {
+  data: Achievement[]
+}
+
+export const ACHIEVEMENT_SORT_OPTIONS = [
+  { text: 'Alphabetically (ASC)', value: 'ALPHA_A_TO_Z' },
+  { text: 'Alphabetically (DES)', value: 'ALPHA_Z_TO_A' }
+] as AchievementSortOption[]
+
+class AchievementState {
+  /**
+   * feature flag for enabled stores
+   */
+  storesEnabled = ['STEAM']
+
+  store = 'STEAM' as AchievementStore
+  private playerStoreId = ''
+
+  // Stats
+  newAchievements = 0
+  totalAchievements = 0
+  totalGames = 0
+  mintedAchievements = 0
+  numFreeMints = 0
+
+  // Summary Achievements
+  summaryAchievements = new Map<string, AchievementData>()
+  currentFilter: AchievementFilter = 'ALL'
+  currentSort: AchievementSortOption = ACHIEVEMENT_SORT_OPTIONS[0]
+  currentStore: AchievementStore = 'STEAM'
+
+  // Individual Achievements
+  individualAchievements = new Map<string, IndividualAchievementData>()
+  currentIndividualSort: AchievementSortOption = ACHIEVEMENT_SORT_OPTIONS[0]
+
+  // Achievements Feature Flag
+  showAchievements = false
+
+  fetching = false
+
+  syncing = false
+
+  constructor() {
+    makeAutoObservable(
+      this,
+      {},
+      {
+        deep: true,
+        proxy: false,
+        name: 'AchievementState'
+      }
+    )
+  }
+
+  init() {
+    window.api
+      .shouldShowAchievements()
+      .then((res) => (this.showAchievements = res))
+  }
+
+  getAchievementsStats = async () => {
+    try {
+      const stats = await window.api.getAchievementsStats({
+        store: this.store,
+        playerStoreId: this.playerStoreId,
+        playerAddress: walletState.address
+      })
+      this.setStats(stats)
+    } catch (err) {
+      console.error(`Error while getting achievement stats ${err}`)
+    }
+  }
+
+  individualAchievementsKey(gameId: string) {
+    const key: IndividualAchievementKey = {
+      gameId,
+      store: this.currentStore,
+      sort: this.currentIndividualSort.value
+    }
+    const keyString = JSON.stringify(key)
+    return keyString
+  }
+
+  async fetchIndividualAchievements({
+    gameId,
+    page,
+    pageSize
+  }: {
+    gameId: string
+    page: number
+    pageSize: number
+  }) {
+    const keyString = this.individualAchievementsKey(gameId)
+
+    /**
+     * Check cache for hit
+     */
+    if (this.individualAchievements.has(keyString)) {
+      return this.individualAchievements.get(keyString)?.data
+    }
+
+    const individualAchievements = await window.api.getIndividualAchievements({
+      gameId: Number(gameId),
+      store: this.store,
+      sort: this.currentIndividualSort.value,
+      page,
+      pageSize,
+      playerStoreId: this.playerStoreId,
+      playerAddress: walletState.address
+    })
+
+    this.individualAchievements.set(keyString, individualAchievements)
+    return individualAchievements.data
+  }
+
+  individualAchievementsForGame(gameId: string) {
+    const keyString = this.individualAchievementsKey(gameId)
+    return this.individualAchievements.get(keyString)
+  }
+
+  fetchMoreSummaryAchievements = async () => {
+    if (this.fetching) {
+      return
+    }
+    if (!this.playerStoreId) {
+      console.error('player store id is not set!')
+      return
+    }
+    this.setFetching(true)
+    const pagesFetched = this.currentSummaryAchievements?.pagesFetched
+    const totalPages = this.currentSummaryAchievements?.totalPages
+    const alreadyFetchedAllPages =
+      pagesFetched !== undefined &&
+      totalPages !== undefined &&
+      pagesFetched >= totalPages
+    if (alreadyFetchedAllPages) {
+      this.setFetching(false)
+      return
+    }
+    const requestKey = this.currentSummaryKey
+
+    const nextPageOfSummaryAchievements =
+      await window.api.getSummaryAchievements({
+        store: this.store,
+        filter: requestKey.filter,
+        sort: requestKey.sort,
+        // api is one indexed not zero indexed
+        page: pagesFetched ? pagesFetched + 1 : 1,
+        playerStoreId: this.playerStoreId,
+        playerAddress: walletState.address
+      })
+
+    const valRef = this.summaryAchievements.get(JSON.stringify(requestKey))
+
+    if (valRef !== undefined) {
+      valRef.data = valRef.data.concat(nextPageOfSummaryAchievements.data)
+      valRef.pagesFetched += 1
+    } else {
+      this.summaryAchievements.set(JSON.stringify(requestKey), {
+        data: nextPageOfSummaryAchievements.data,
+        pagesFetched: 1,
+        totalPages: nextPageOfSummaryAchievements.totalPages
+      })
+    }
+    this.setFetching(false)
+  }
+
+  /**
+   * Necessary since strict-mode is enabled in mobx
+   */
+  setFetching = (val: boolean) => {
+    runInAction(() => {
+      this.fetching = val
+    })
+  }
+
+  private get currentSummaryKey(): AchievementRequestKey {
+    return {
+      filter: this.currentFilter,
+      sort: this.currentSort.value,
+      store: this.currentStore
+    }
+  }
+
+  get currentSummaryAchievements() {
+    const key = this.currentSummaryKey
+    return this.summaryAchievements.get(JSON.stringify(key))
+  }
+
+  get summaryAchievementsToDisplay() {
+    return this.currentSummaryAchievements?.data
+  }
+
+  /**
+   * Fetch for current key if no game summaries have been fetched
+   */
+  initGameSummaryKey() {
+    if (this.summaryAchievementsToDisplay === undefined) {
+      this.fetchMoreSummaryAchievements()
+    }
+  }
+
+  setFilter(filter: AchievementFilter) {
+    this.currentFilter = filter
+    this.initGameSummaryKey()
+  }
+
+  setSort(sort: AchievementSortOption) {
+    this.currentSort = sort
+    this.initGameSummaryKey()
+  }
+
+  syncAchievements = async (store: AchievementStore) => {
+    if (!this.playerStoreId) {
+      console.error('Player store id is not set!')
+      return
+    }
+    if (this.syncing) {
+      return
+    }
+    this.setSyncing(true)
+    try {
+      await window.api.syncAchievements({
+        store,
+        playerStoreId: this.playerStoreId,
+        playerAddress: walletState.address
+      })
+    } catch (err) {
+      console.error(err)
+    }
+    this.setSyncing(false)
+  }
+
+  /**
+   * Necessary since strict-mode is enabled in mobx
+   */
+  setSyncing = (val: boolean) => {
+    runInAction(() => {
+      this.syncing = val
+    })
+  }
+
+  setStats = (state: AchievementsStats) => {
+    this.newAchievements = state.newAchievements
+    this.totalAchievements = state.totalAchievements
+    this.totalGames = state.totalGames
+    this.mintedAchievements = state.mintedAchievements
+    this.numFreeMints = state.numFreeMints
+  }
+
+  setStore = (store: AchievementStore) => {
+    this.store = store
+  }
+
+  setPlayerStoreId = (playerStoreId: string) => {
+    this.playerStoreId = playerStoreId
+    this.summaryAchievements.clear()
+  }
+
+  get getPlayerStoreId() {
+    return this.playerStoreId
+  }
+
+  get filterDisplayName() {
+    const activeFilter = this.currentFilter
+    if (activeFilter === 'NEW') return 'new'
+    if (activeFilter === 'MINTED') return 'minted'
+    return 'all'
+  }
+
+  getFilterNameFromDisplayName(displayName: string) {
+    let newFilter = 'ALL' as AchievementFilter
+    if (displayName === 'new') newFilter = 'NEW'
+    if (displayName === 'minted') newFilter = 'MINTED'
+    return newFilter
+  }
+
+  getSummaryAchievement(id: string) {
+    return this.summaryAchievementsToDisplay?.find(
+      (val) => val.gameId.toString() === id
+    )
+  }
+}
+
+export default new AchievementState()
