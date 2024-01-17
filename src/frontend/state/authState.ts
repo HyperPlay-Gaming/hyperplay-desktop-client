278833a0d11fbbcb629dd01d1d3894177a118053
Elio Briceño <eliobricenov@gmail.com>
2024-01-17 17:13:32 -0400

    chore(auth): remove unused methods

--- src/frontend/state/authState.ts
+++ src/frontend/state/authState.ts
@@ -2,9 +2,7 @@ import { onboardingStore } from 'frontend/helpers/electronStores'
 import { makeAutoObservable } from 'mobx'
 
 class AuthState {
-  private qaModeActive = false
   private signInModalOpen = false
-  private signedIn = false
   private pendingSignatureRequest = false
   authToken = ''
 
@@ -25,28 +23,12 @@ class AuthState {
     return this.signInModalOpen
   }
 
-  get isSignedIn() {
-    return this.signedIn
-  }
-
-  get isQaModeActive() {
-    return this.qaModeActive
-  }
-
   openSignInModal() {
     this.signInModalOpen = true
   }
 
   closeSignInModal() {
     this.signInModalOpen = false
-  }
-
-  activateQaMode() {
-    this.qaModeActive = true
-  }
-
-  setSignedIn() {
-    this.signedIn = true
   }
 
   setPendingSignatureRequest(pending: boolean) {
