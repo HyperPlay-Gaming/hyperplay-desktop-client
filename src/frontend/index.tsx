e116992b5b3253daecf0a1453a2f191e442a7a81
Redeemer Pace | Goku | Game Dev <75917402+red-game-dev@users.noreply.github.com>
2024-02-05 19:49:44 +0100

    [TECH]: add support for ui lib with Mantine v7  (#660)
    * feat: Added mantine provider
    
    * chore: Update yarn lock
    
    * fix: Fix yarn lock was missing check-disk-space
    
    * fix Tabs
    
    * chore: replace hp provider with mantine
    
    * chore: Upgrade core, hooks, carousel for mantine
    
    * chore: Added yarn.lock
    
    * fix: Made same version for autoplay, react carousel as ui lib
    
    * chore: use new hp lib version
    
    * chore: add mantine css classes
    
    * chore: use ui lib with fix
    
    * style(tabs): use tab from ui lib instead for matines
    
    * style: update remaining broken styles
    
    * chore: use fixed mantine dropzone version
    
    * chore: use fixed mantine dropzone version
    
    * chore: bump ui package
    
    * chore: fix error
    
    * style(library): fix topbar styles
    
    * chore: use latest ui lib
    
    * chore: run prettier
    
    * chore: remove local lavamoat config
    
    * chore: update lock
    
    * chore: bump mantine
    
    * chore: bump mantine
    
    ---------
    
    Co-authored-by: Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
    Co-authored-by: Elio Briceño <eliobricenov@gmail.com>

--- src/frontend/index.tsx
+++ src/frontend/index.tsx
@@ -1,3 +1,4 @@
+import { HyperPlayDesignProvider } from '@hyperplay/ui'
 import { I18nextProvider, initReactI18next } from 'react-i18next'
 import HttpApi from 'i18next-http-backend'
 import React, { Suspense } from 'react'
@@ -5,7 +6,11 @@ import { createRoot } from 'react-dom/client'
 import i18next from 'i18next'
 import { initGamepad } from './helpers/gamepad'
 
+import '@mantine/carousel/styles.css'
+import '@mantine/core/styles.css'
+
 // keep @hyperplay/ui/index.css before index.scss until after frontend design rework
+// import HyperPlay styles after mantine to override their defaults with our design system
 import '@hyperplay/ui/style.css'
 import './index.scss'
 import Loading from './screens/Loading'
@@ -18,12 +23,12 @@ import { defaultThemes } from './components/UI/ThemeSelector'
 import '@fontsource/rajdhani'
 import '@fontsource/barlow'
 import StoreController from './store'
-
-initOnlineMonitor()
 import ViewManager from './ViewManager'
 import SentryHandler from './SentryHandler'
 import { asyncWithLDProvider } from 'launchdarkly-react-client-sdk'
 import { QueryClient, QueryClientProvider } from 'react-query'
+
+initOnlineMonitor()
 
 window.addEventListener('error', (ev: ErrorEvent) => {
   window.api.logError(ev.error.stack)
@@ -122,14 +127,16 @@ const renderApp = async () => {
       <StoreController />
       <QueryClientProvider client={queryClient}>
         <LDProvider>
-          <GlobalState>
-            <SentryHandler />
-            <I18nextProvider i18n={i18next}>
-              <Suspense fallback={<Loading />}>
-                <ViewManager />
-              </Suspense>
-            </I18nextProvider>
-          </GlobalState>
+          <HyperPlayDesignProvider forceColorScheme="dark">
+            <GlobalState>
+              <SentryHandler />
+              <I18nextProvider i18n={i18next}>
+                <Suspense fallback={<Loading />}>
+                  <ViewManager />
+                </Suspense>
+              </I18nextProvider>
+            </GlobalState>
+          </HyperPlayDesignProvider>
         </LDProvider>
       </QueryClientProvider>
     </React.StrictMode>
