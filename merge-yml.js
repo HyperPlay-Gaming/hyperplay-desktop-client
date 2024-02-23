e156b0070731cc08d8ed786ac90929a286f99651
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-02-23 21:09:42 +0000

    Merge YML files to create latest-mac.yml using node script

---
+++ merge-yml.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const yaml = require('js-yaml')
+
+const x64Content = fs.readFileSync('dist-x64/latest-mac-x64.yml', 'utf8')
+const arm64Content = fs.readFileSync('dist-arm64/latest-mac-arm64.yml', 'utf8')
+
+const x64Data = yaml.load(x64Content)
+const arm64Data = yaml.load(arm64Content)
+
+// Merge files field from both x64 and arm64
+const mergedFiles = [...(x64Data.files || []), ...(arm64Data.files || [])]
+
+// Combine other fields
+const combinedData = {
+  version: x64Data.version || '0.0.0',
+  files: mergedFiles,
+  path: x64Data.path || '',
+  sha512: x64Data.sha512 || '',
+  releaseDate: x64Data.releaseDate || ''
+}
+
+const resultYaml = yaml.dump(combinedData, { lineWidth: 120 })
+
+fs.writeFileSync('dist/latest-mac.yml', resultYaml, 'utf8')
