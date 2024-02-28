c1e6412da47a3bf9402e356699354ca8645f6197
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2024-02-27 18:07:56 -0800

    uncheck toggle by default on mac

--- src/frontend/screens/Settings/components/AutoLaunchHyperPlay.tsx
+++ src/frontend/screens/Settings/components/AutoLaunchHyperPlay.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { ToggleSwitch } from 'frontend/components/UI'
 import useSetting from 'frontend/hooks/useSetting'
 import { useTranslation } from 'react-i18next'
 
 const AutoLaunchHyperPlay = () => {
   const { t } = useTranslation()
+  
   const [autoLaunchHyperPlay, setAutoLaunchHyperPlay] = useSetting(
     'autoLaunchHyperPlay',
     true
   )
+
+  useEffect(()=>{
+    async function init(){
+      const platform = await window.api.getPlatform()
+      // currently set as opt in on mac
+      if (platform === 'darwin'){
+        setAutoLaunchHyperPlay(false)
+      }
+    }
+    init()
+  })
 
   return (
     <ToggleSwitch
