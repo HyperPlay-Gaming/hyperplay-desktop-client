d18d511a9db941083f100e97555929bc99f0e565
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-02-27 17:26:45 -0800

    disable opt in on start for mac

--- src/backend/utils/auto_launch.ts
+++ src/backend/utils/auto_launch.ts
@@ -1,5 +1,6 @@
 import AutoLaunch from 'auto-launch'
 import { GlobalConfig } from 'backend/config'
+import { isMac } from 'backend/constants'
 import { ipcMain } from 'electron'
 
 export const hyperPlayAutoLauncher = new AutoLaunch({
@@ -28,8 +29,9 @@ ipcMain.handle('updateAutoLaunch', async () => {
 const settings = GlobalConfig.get().getSettings()
 const { autoLaunchHyperPlay } = settings
 
-// default to true on fresh install
-if (autoLaunchHyperPlay === undefined) {
+// default to true on fresh install. except for on Mac
+// see: https://github.com/HyperPlay-Gaming/hyperplay-desktop-client/issues/770
+if (autoLaunchHyperPlay === undefined && !isMac) {
   updateAutoLaunch(true)
   GlobalConfig.get().setSetting('autoLaunchHyperPlay', true)
 }
