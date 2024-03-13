e27f5fe0ec43eef27a5f131ce3bd814fcc825f42
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-03-13 13:46:28 +0000

    feat: notify when setting up CL

--- src/backend/utils.ts
+++ src/backend/utils.ts
@@ -78,7 +78,7 @@ import {
 } from './storeManagers/nile/electronStores'
 
 import makeClient from 'discord-rich-presence-typescript'
-import { showDialogBoxModalAuto } from './dialog/dialog'
+import { notify, showDialogBoxModalAuto } from './dialog/dialog'
 import { getMainWindow, sendFrontendMessage } from './main_window'
 import { GlobalConfig } from './config'
 import { GameConfig } from './game_config'
@@ -1042,6 +1042,14 @@ export async function downloadDefaultWine() {
       progress
     })
   }
+
+  notify({
+    title: i18next.t('notification.wine-download.title', 'Compatibility Layer'),
+    body: i18next.t(
+      'notification.wine-download.message',
+      'Setting up the default compatibility layer'
+    )
+  })
 
   const result = await installWineVersion(
     release,
