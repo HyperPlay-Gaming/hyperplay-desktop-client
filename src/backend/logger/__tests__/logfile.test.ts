Heroic-Games-Launcher/HeroicGamesLauncher

[UX] Show runners and installed games logs in Settings > Logs (#3147)
* Show runners and games in Settings > Log

* Fix specs

* Fix specs

* Remove unused translation

* Log revamp: add log sanitizer function to callRunner

* Fix width/height of log box, add underline to current log

src/backend/logger/__tests__/logfile.test.ts
@@ -127,17 +127,12 @@ describeSkipOnWindows('logger/logfile.ts', () => {
   })
 
   test('getLogFile all possible values', () => {
-    // get global current logfile
-    expect(logfile.getLogFile({})).toBe('current.log')
-    // get global last logfile
-    expect(logfile.getLogFile({ defaultLast: true })).toBe('last.log')
-
+    expect(logfile.getLogFile('heroic')).toMatch(/-heroic.log$/)
+    expect(logfile.getLogFile('legendary')).toMatch(/-legendary.log$/)
+    expect(logfile.getLogFile('gogdl')).toMatch(/-gogdl.log$/)
+    expect(logfile.getLogFile('nile')).toMatch(/-nile.log$/)
     // get game log
-    expect(logfile.getLogFile({ appName: 'MyApp' })).toBe(
-      '/tmp/appData/heroic/GamesConfig/MyApp-lastPlay.log'
-    )
-    // get game log and isDefaultLast has no impact
-    expect(logfile.getLogFile({ appName: 'MyApp', defaultLast: true })).toBe(
+    expect(logfile.getLogFile('MyApp')).toBe(
       '/tmp/appData/heroic/GamesConfig/MyApp-lastPlay.log'
     )
   })

HyperPlay-Gaming/hyperplay-desktop-client

src/backend/logger/__tests__/logfile.test.ts
@@ -135,21 +135,16 @@ describe('logger/logfile.ts', () => {
     expect(graceful_fs.existsSync(yearOutdatedLogFile)).toBeFalsy()
   }) */
 
-  /*   test('getLogFile all possible values', () => {
-    // get global current logfile
-    expect(logfile.getLogFile({})).toBe('current.log')
-    // get global last logfile
-    expect(logfile.getLogFile({ defaultLast: true })).toBe('last.log')
-
+  test('getLogFile all possible values', () => {
+    expect(logfile.getLogFile('heroic')).toMatch(/-hyperplay.log$/)
+    expect(logfile.getLogFile('legendary')).toMatch(/-legendary.log$/)
+    expect(logfile.getLogFile('gogdl')).toMatch(/-gogdl.log$/)
+    expect(logfile.getLogFile('nile')).toMatch(/-nile.log$/)
     // get game log
-    expect(logfile.getLogFile({ appName: 'MyApp' })).toBe(
-      '/tmp/appData/hyperplay/GamesConfig/MyApp-lastPlay.log'
+    expect(logfile.getLogFile('MyApp')).toBe(
+      '/tmp/appData/heroic/GamesConfig/MyApp-lastPlay.log'
     )
-    // get game log and isDefaultLast has no impact
-    expect(logfile.getLogFile({ appName: 'MyApp', defaultLast: true })).toBe(
-      '/tmp/appData/hyperplay/GamesConfig/MyApp-lastPlay.log'
-    )
-  }) */
+  })
 
   test('appendMessageToLogFile success', () => {
     const appendFileSyncSpy = jest