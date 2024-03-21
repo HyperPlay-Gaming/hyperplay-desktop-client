d21467c324877fe4802ffebc059cb34062321fb6
Flavio F Lima <26871415+flavioislima@users.noreply.github.com>
2024-03-21 15:07:46 +0000

    Revert "fix: getPCIds failing on windows"
    This reverts commit ab42cfe07ad2fda29c07b1d7fc5c1b08c9bdae49.

--- src/backend/utils/systeminfo/gpu/pci_ids.ts
+++ src/backend/utils/systeminfo/gpu/pci_ids.ts
@@ -2,16 +2,14 @@
  * Contains helper functions to work with the `pci.ids` file
  * ({@link https://pci-ids.ucw.cz})
  */
-import { join } from 'path'
+import axios, { AxiosError } from 'axios'
+import path from 'path'
 
+import { downloadFile, DAYS } from '../../inet/downloader'
 import { toolsPath } from 'backend/constants'
 
 import type { PartialGpuInfo } from './index'
 import type { GPUInfo } from '../index'
-import { LogPrefix, logError } from 'backend/logger/logger'
-import { downloadFile } from 'backend/utils'
-import { createAbortController } from 'backend/utils/aborthandler/aborthandler'
-import { existsSync, readFileSync } from 'graceful-fs'
 
 const pciIdsMap: Record<
   string,
@@ -27,26 +25,21 @@ const pciIdsMap: Record<
   }
 > = {}
 
-async function getPciIds(): Promise<typeof pciIdsMap | null> {
+async function getPicIds(): Promise<typeof pciIdsMap | null> {
   if (Object.keys(pciIdsMap).length !== 0) return pciIdsMap
 
-  await downloadFile(
-    'https://pci-ids.ucw.cz/v2.2/pci.ids',
-    toolsPath,
-    'pci.ids',
-    createAbortController('pci.ids')
-  )
+  const pciIdsFile = await downloadFile('https://pci-ids.ucw.cz/v2.2/pci.ids', {
+    axiosConfig: {
+      responseType: 'text'
+    },
+    writeToFile: path.join(toolsPath, 'pci.ids'),
+    maxCache: 30 * DAYS
+  }).catch((error) => error as AxiosError)
+  if (axios.isAxiosError(pciIdsFile)) return null
 
   let currentVendor: string | null = null
   let currentDevice: string | null = null
-
-  const pciIdsFile = join(toolsPath, 'pci.ids')
-
-  if (!existsSync(pciIdsFile)) return null
-
-  const pciIdsFileContent = readFileSync(pciIdsFile, 'utf-8')
-
-  for (const line of pciIdsFileContent.split('\n')) {
+  for (const line of pciIdsFile.split('\n')) {
     // Skip comments and empty lines
     if (line.startsWith('#')) continue
     if (line === '') continue
@@ -96,36 +89,28 @@ async function getPciIds(): Promise<typeof pciIdsMap | null> {
 async function populateDeviceAndVendorName(
   partialGpus: PartialGpuInfo[]
 ): Promise<GPUInfo[]> {
-  try {
-    const pciIds = await getPciIds()
-    if (pciIds === null) return partialGpus
+  const pciIds = await getPicIds()
+  if (pciIds === null) return partialGpus
 
-    const fullGpuInfo: GPUInfo[] = []
-    for (const gpu of partialGpus) {
-      const vendorId = gpu.vendorId.toLowerCase()
-      const deviceId = gpu.deviceId.toLowerCase()
-      const subvendorId = gpu.subvendorId?.toLowerCase()
-      const subdeviceId = gpu.subdeviceId?.toLowerCase()
-      const vendor = pciIds[vendorId]
-      const device = pciIds[vendorId]?.devices[deviceId]
-      const subsystem =
-        pciIds[vendorId]?.devices[deviceId]?.subsystems[
-          `${subvendorId} ${subdeviceId}`
-        ]
-      fullGpuInfo.push({
-        ...gpu,
-        deviceString: subsystem ?? device?.deviceName,
-        vendorString: vendor?.vendorName
-      })
-    }
-    return fullGpuInfo
-  } catch (error) {
-    logError(
-      `Failed to populate device and vendor name: ${error}`,
-      LogPrefix.Backend
-    )
-    return partialGpus
+  const fullGpuInfo: GPUInfo[] = []
+  for (const gpu of partialGpus) {
+    const vendorId = gpu.vendorId.toLowerCase()
+    const deviceId = gpu.deviceId.toLowerCase()
+    const subvendorId = gpu.subvendorId?.toLowerCase()
+    const subdeviceId = gpu.subdeviceId?.toLowerCase()
+    const vendor = pciIds[vendorId]
+    const device = pciIds[vendorId]?.devices[deviceId]
+    const subsystem =
+      pciIds[vendorId]?.devices[deviceId]?.subsystems[
+        `${subvendorId} ${subdeviceId}`
+      ]
+    fullGpuInfo.push({
+      ...gpu,
+      deviceString: subsystem ?? device?.deviceName,
+      vendorString: vendor?.vendorName
+    })
   }
+  return fullGpuInfo
 }
 
 export { populateDeviceAndVendorName }
