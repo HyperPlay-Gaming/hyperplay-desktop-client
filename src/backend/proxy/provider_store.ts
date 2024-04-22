24739c3493bb3fec5c6ef434de3d8bfbb534e3b3
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-04-22 16:54:33 -0700

    fix provider store name

--- src/backend/proxy/provider_store.ts
+++ src/backend/proxy/provider_store.ts
@@ -2,7 +2,7 @@ import { TypeCheckedStoreBackend } from 'backend/electron_store'
 
 const defaultProviderStore = new TypeCheckedStoreBackend('providerMetadata', {
   cwd: 'store',
-  name: 'x'
+  name: 'default_provider_store'
 })
 
 export default defaultProviderStore
