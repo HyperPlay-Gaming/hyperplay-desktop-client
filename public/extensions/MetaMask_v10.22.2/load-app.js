307bae0e6c107e37127e6a3e5ecf331005fc1326
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-14 16:37:03 -0700

    update mm (#877)

--- public/extensions/MetaMask_v10.22.2/load-app.js
+++ public/extensions/MetaMask_v10.22.2/load-app.js
@@ -4,7 +4,7 @@
 setTimeout(() => {
   // eslint-disable-next-line spaced-comment
   const scriptsToLoad = [
-    ...["./snow.js","./use-snow.js","./globalthis.js","./sentry-install.js","./runtime-lavamoat.js","./lockdown-more.js","./policy-load.js","./common-0.js","./common-1.js","./common-2.js","./common-3.js","./common-4.js","./common-5.js","./common-6.js","./common-7.js","./ui-0.js","./ui-1.js","./ui-2.js","./ui-3.js","./ui-4.js","./ui-5.js","./ui-6.js","./ui-7.js","./ui-8.js","./ui-9.js","./ui-10.js","./ui-11.js"]
+    ...["./scripts/snow.js","./scripts/use-snow.js","./scripts/sentry-install.js","./scripts/runtime-lavamoat.js","./scripts/lockdown-more.js","./scripts/policy-load.js","./common-0.js","./common-1.js","./common-2.js","./common-3.js","./common-4.js","./common-5.js","./common-6.js","./common-7.js","./common-8.js","./ui-0.js","./ui-1.js","./ui-2.js","./ui-3.js","./ui-4.js","./ui-5.js","./ui-6.js","./ui-7.js","./ui-8.js","./ui-9.js","./ui-10.js"]
   ];
 
   const loadScript = (src) => {
