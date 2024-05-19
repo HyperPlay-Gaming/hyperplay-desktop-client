d181be85855b810fad9bfc97e2af5bd868e7c83d
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-18 20:16:18 -0700

    ignore buildResetExtension

--- buildResetExtension.js
+++ buildResetExtension.js
@@ -1,13 +1,18 @@
-const { spawnSync } = require("child_process");
-const { existsSync } = require("fs");
+const { spawnSync } = require('child_process')
+const { existsSync } = require('fs')
 const process = require('process')
 
 const [nodeExe, scriptPath, ...args] = process.argv
 
-const resetExtensionScript = './node_modules/@hyperplay/extension-importer/src/resetExtension.js'
-if (existsSync(resetExtensionScript)){
-    spawnSync('pkg', [resetExtensionScript, '--out-path', './public/extensions/resetExecutables', ...args])
-}
-else {
-    console.warn('Reset extension script could not be found')
+const resetExtensionScript =
+  './node_modules/@hyperplay/extension-importer/src/resetExtension.js'
+if (existsSync(resetExtensionScript)) {
+  spawnSync('pkg', [
+    resetExtensionScript,
+    '--out-path',
+    './public/extensions/resetExecutables',
+    ...args
+  ])
+} else {
+  console.warn('Reset extension script could not be found')
 }
