5c0091453462c9d8f0fa53a35d4e84a704bf5327
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-05-18 19:40:52 -0700

    prettier

--- src/frontend/screens/Onboarding/walletSelection/screens/import/components/BrowserPackageManagerImportOptions/index.tsx
+++ src/frontend/screens/Onboarding/walletSelection/screens/import/components/BrowserPackageManagerImportOptions/index.tsx
@@ -17,7 +17,8 @@ export function BrowserPackageManagerImportOption({
     importOptions[browserSelected]![pkgManager]
   function getProfileBgColor(profile: BrowserProfile) {
     return (
-      '#' + (profile.imageBackgroundColor ?? DEFAULT_BROWSER_PROFILE_IMPORT_COLOR)
+      '#' +
+      (profile.imageBackgroundColor ?? DEFAULT_BROWSER_PROFILE_IMPORT_COLOR)
     )
   }
   return (
