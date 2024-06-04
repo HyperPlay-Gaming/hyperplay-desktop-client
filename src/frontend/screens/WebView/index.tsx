be94bfeac8509722f6d89964c5ff11162c75aa76
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-06-04 19:23:17 +0100

    Revert "Revert "chore: REVERT BEFORE MERGING - test store URL""
    This reverts commit 93c65fabe7a92f32e645141191837035424f4c08.

--- src/frontend/screens/WebView/index.tsx
+++ src/frontend/screens/WebView/index.tsx
@@ -32,7 +32,7 @@ import { getGameInfo } from 'frontend/helpers'
 
 function urlIsHpUrl(url: string) {
   const urlToTest = new URL(url)
-  return urlToTest.hostname === 'store.hyperplay.xyz'
+  return urlToTest.hostname === 'hyperplay-store-git-feat-libfromstore-hyperplay.vercel.app'
 }
 
 function shouldInjectProvider(url: string) {
