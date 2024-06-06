727726e59f7544bb449898a52c0d65726c310370
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-06-06 16:26:32 -0700

    update react-query hook

--- src/frontend/hooks/useGetHyperPlayListings.ts
+++ src/frontend/hooks/useGetHyperPlayListings.ts
@@ -1,24 +1,23 @@
 import { HyperPlayRelease } from 'common/types'
-import { useQuery, useQueryClient } from 'react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 
 export default function useGetHyperPlayListings() {
   const queryClient = useQueryClient()
   const queryKey = `getHyperPlayListings`
-  const query = useQuery<Record<string, HyperPlayRelease> | null>(
-    queryKey,
-    async () => {
+  const query = useQuery<Record<string, HyperPlayRelease> | null>({
+    queryKey: [queryKey],
+    queryFn: async () => {
       const response = await window.api.getHyperPlayListings()
       if (!response) return null
       return response
     },
-    {
-      refetchOnWindowFocus: false
-    }
-  )
+    refetchOnWindowFocus: false
+  })
 
   return {
     data: query,
     isLoading: query.isLoading || query.isFetching,
-    invalidateQuery: async () => queryClient.invalidateQueries(queryKey)
+    invalidateQuery: async () =>
+      queryClient.invalidateQueries({ queryKey: [queryKey] })
   }
 }
