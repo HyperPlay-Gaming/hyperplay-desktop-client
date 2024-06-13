0695f221a601077d7681fbff254ab8bfc409e4a0
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-06-12 21:33:51 -0700

    fixes

--- src/backend/overlay/index.ts
+++ src/backend/overlay/index.ts
@@ -4,14 +4,21 @@ import { HyperPlayAPI } from '@hyperplay/utils'
 export async function getHpOverlay() {
   try {
     const hpOverlay = await import('@hyperplay/overlay')
+    if (!Object.hasOwn(hpOverlay, 'toggleOverlay')) {
+      return undefined
+    }
     return hpOverlay
   } catch (err) {
-    logError(`Error initializing overlay ${err}`, LogPrefix.HyperPlay)
+    logError(`Error getting overlay ${err}`, LogPrefix.HyperPlay)
   }
   return undefined
 }
 
 export const initOverlay = async function (api: HyperPlayAPI) {
-  const hpOverlay = await getHpOverlay()
-  hpOverlay?.initOverlay(api)
+  try {
+    const hpOverlay = await getHpOverlay()
+    hpOverlay?.initOverlay(api)
+  } catch (err) {
+    logError(`Error initializing overlay ${err}`, LogPrefix.HyperPlay)
+  }
 }
