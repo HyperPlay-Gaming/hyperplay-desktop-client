9c94c097e234c1c06ca7c20a2d0e8cac15a95092
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-07-08 20:42:43 -0700

    update log

--- src/frontend/components/UI/QuestsViewer/components/QuestDetailsWrapper/rewards/claimPoints.ts
+++ src/frontend/components/UI/QuestsViewer/components/QuestDetailsWrapper/rewards/claimPoints.ts
@@ -2,5 +2,5 @@ import { Reward } from 'common/types'
 
 export async function claimPoints(reward: Reward) {
   const result = await window.api.claimQuestPointsReward(reward.id.toString())
-  console.log('claim points result ', result)
+  console.log('for reward id ', reward.id, ' claim points result ', result)
 }
