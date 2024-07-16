a028ebbf0d221cf419def4fe031a2671cda3eeb5
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-07-15 18:24:25 -0700

    add log in alert to quests page and overlay

--- src/frontend/components/UI/QuestsViewer/index.tsx
+++ src/frontend/components/UI/QuestsViewer/index.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react'
 import styles from './index.module.scss'
 import { QuestLogWrapper } from './components/QuestLogWrapper'
 import { QuestDetailsWrapper } from './components/QuestDetailsWrapper'
+import { Alert } from '@hyperplay/ui'
+import useAuthSession from 'frontend/hooks/useAuthSession'
 
 export interface QuestsViewerProps {
   projectId: string
@@ -9,15 +11,30 @@ export interface QuestsViewerProps {
 
 export function QuestsViewer({ projectId: appName }: QuestsViewerProps) {
   const [selectedQuestId, setSelectedQuestId] = useState<number | null>(null)
+  const { isSignedIn } = useAuthSession()
+
+  let alertComponent = null
+  if (!isSignedIn) {
+    alertComponent = (
+      <Alert
+        className={styles.alert}
+        message="Be sure to log into HyperPlay to maintain your play streak progress."
+        variant="warning"
+      />
+    )
+  }
 
   return (
-    <div className={styles.questsViewerContainer}>
-      <QuestLogWrapper
-        projectId={appName}
-        selectedQuestId={selectedQuestId}
-        setSelectedQuestId={setSelectedQuestId}
-      />
-      <QuestDetailsWrapper selectedQuestId={selectedQuestId} />
+    <div className={styles.root}>
+      {alertComponent}
+      <div className={styles.questsViewerContainer}>
+        <QuestLogWrapper
+          projectId={appName}
+          selectedQuestId={selectedQuestId}
+          setSelectedQuestId={setSelectedQuestId}
+        />
+        <QuestDetailsWrapper selectedQuestId={selectedQuestId} />
+      </div>
     </div>
   )
 }
