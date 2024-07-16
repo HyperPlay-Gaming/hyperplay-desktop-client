f61d5d5e7ee86e2ed221cbe72f06bb11cd66c296
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-07-16 11:13:27 -0700

    [Feat] add log in alert to quests page and overlay (#939)
    * add log in alert to quests page and overlay
    
    * fix styling with no alert component

--- src/frontend/screens/Quests/index.tsx
+++ src/frontend/screens/Quests/index.tsx
@@ -2,15 +2,39 @@ import React, { useState } from 'react'
 import { QuestsSummaryTableWrapper } from './components/QuestsSummaryTableWrapper'
 import { QuestDetailsViewPlayWrapper } from './components/QuestDetailsViewPlay'
 import styles from './index.module.scss'
-import { Background } from '@hyperplay/ui'
+import { Alert, Background } from '@hyperplay/ui'
 import classNames from 'classnames'
+import useAuthSession from 'frontend/hooks/useAuthSession'
 
 export function QuestsPage() {
   const [selectedQuestId, setSelectedQuestId] = useState<number | null>(null)
+  const { isSignedIn } = useAuthSession()
+
+  let alertComponent = null
+  const showAlert = !isSignedIn
+  let contentWithAlertClass = styles['no-alert']
+  if (showAlert) {
+    contentWithAlertClass = styles['with-alert']
+    alertComponent = (
+      <Alert
+        className={styles.alert}
+        message="Be sure to log into HyperPlay to maintain your play streak progress."
+        variant="warning"
+      />
+    )
+  }
+
   return (
     <>
       <Background style={{ position: 'absolute' }}></Background>
-      <div className={classNames('contentContainer', styles.root)}>
+      <div
+        className={classNames(
+          'contentContainer',
+          contentWithAlertClass,
+          styles.root
+        )}
+      >
+        {alertComponent}
         <QuestsSummaryTableWrapper
           setSelectedQuestId={setSelectedQuestId}
           selectedQuestId={selectedQuestId}
