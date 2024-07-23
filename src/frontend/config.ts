3a730dd6ffa72e543d949cdcd72797fb5d50be62
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2024-07-23 11:01:24 -0400

    feat: add all chains from @hyperplay/chains to wagmi config (#949)
    * feat: add all chains from @hyperplay/chains to wagmi config
    
    * tech: use function from chains package

--- src/frontend/config.ts
+++ src/frontend/config.ts
@@ -1,11 +1,31 @@
 import { createConfig, http } from 'wagmi'
-import { mainnet, polygon, sepolia } from 'wagmi/chains'
+import { Chain, hardhat, mainnet, polygon } from 'wagmi/chains'
+import { chainMap, parseChainMetadataToViemChain } from '@hyperplay/chains'
+
+let chainsToSupport: Chain[] = []
+const transports = {}
+
+for (const chainId in chainMap) {
+  const chainMetadata = chainMap[chainId]
+  try {
+    const chain = parseChainMetadataToViemChain(chainMetadata)
+    // @ts-expect-error: the function is valid, there is just a mismatch in the types due to different versions of viem
+    chainsToSupport.push(chain)
+    transports[chain.id] = http(chainMetadata.chain.rpc[0])
+    // eslint-disable-next-line no-empty
+  } catch (error) {}
+}
+
+// add hardhat chain for development
+chainsToSupport = chainsToSupport.filter((chain) => chain.id !== hardhat.id)
+chainsToSupport.push(hardhat)
 
 export const config = createConfig({
-  chains: [mainnet, sepolia, polygon],
+  // @ts-expect-error: Chain[] is a valid type for chains but wagmi a constant assertion which we can't do since the array is dynamic
+  chains: chainsToSupport,
   transports: {
-    [mainnet.id]: http(mainnet.rpcUrls.default.http[0]),
-    [sepolia.id]: http(sepolia.rpcUrls.default.http[0]),
-    [polygon.id]: http(polygon.rpcUrls.default.http[0])
+    ...transports,
+    [mainnet.id]: http('https://rpc.valist.io/mainnet'),
+    [polygon.id]: http('https://rpc.valist.io/polygon')
   }
 })
