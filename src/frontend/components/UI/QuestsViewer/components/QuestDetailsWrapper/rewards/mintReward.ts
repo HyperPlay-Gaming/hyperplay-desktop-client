4f598cf2141107390606ef8a6e32ae2fd2ff4313
Elio Briceño <38574891+eliobricenov@users.noreply.github.com>
2024-07-26 16:06:35 -0400

    feat(quest):  add balance check and claim error CTA message  (#953)
    * tech(overlay): add no wallet connected text
    
    * tech(quest): add balance check and error CTA message
    
    * tech(quest): use consistent pattern
    
    * tech(i18n): only add new fields
    
    * tech(i18n): only add new fields
    
    * tech(i18n): only add new fields
    
    * tech(i18n): only add new fields
    
    * tech(i18n): rename value
    
    * Revert "tech(i18n): rename value"
    
    This reverts commit d87e56ac4f7f6ca4a195dcfd3caab58d7267bcbc.
    
    * tech(overlay): revert unwanted change
    
    * tech(quest): replace early returns with throws
    
    * tech(quest): add log on error
    
    * tech(quest): address PR comments
    
    * fix(quest): ceil gas multiplication
    
    * tech(quest): rename
    
    * tech(quest-detail): reset errors on selected quest id change

--- src/frontend/components/UI/QuestsViewer/components/QuestDetailsWrapper/rewards/mintReward.ts
+++ src/frontend/components/UI/QuestsViewer/components/QuestDetailsWrapper/rewards/mintReward.ts
@@ -1,33 +1,33 @@
 import { DepositContract, Reward, RewardClaimSignature } from 'common/types'
 import { getAmount } from '@hyperplay/utils'
 import { questRewardAbi } from 'frontend/abis/RewardsAbi'
-import { SwitchChainMutateAsync, WriteContractMutate } from 'wagmi/query'
+import { WriteContractMutate } from 'wagmi/query'
 import { Config } from 'wagmi'
 
 export async function mintReward({
   reward,
   questId,
   address,
-  switchChainAsync,
   writeContract
 }: {
   reward: Reward
   questId: number
   address: `0x${string}`
-  switchChainAsync: SwitchChainMutateAsync<Config, unknown>
   writeContract: WriteContractMutate<Config, unknown>
 }) {
   if (reward.chain_id === null) {
-    console.error('chain id is not set for reward when trying to mint')
-    return
+    throw Error('chain id is not set for reward when trying to mint')
   }
-  await switchChainAsync({ chainId: reward.chain_id })
+
   const isERC1155Reward =
     reward.reward_type === 'ERC1155' && reward.token_ids.length === 1
+
   let tokenId: number | undefined = undefined
+
   if (isERC1155Reward) {
     tokenId = reward.token_ids[0].token_id
   }
+
   const sig: RewardClaimSignature = await window.api.getQuestRewardSignature(
     address,
     reward.id,
@@ -36,68 +36,89 @@ export async function mintReward({
 
   const depositContracts: DepositContract[] =
     await window.api.getDepositContracts(questId)
+
   const depositContractAddress = depositContracts.find(
     (val) => val.chain_id === reward.chain_id
   )?.contract_address
+
   if (depositContractAddress === undefined) {
-    console.error(
+    throw Error(
       `Deposit contract address undefined for quest ${questId} and chain id ${reward.chain_id}`
     )
-    return
   }
+
+  const logError = (error: Error) => {
+    window.api.logError(`Error minting reward: ${error.message}`)
+  }
+
   if (
     reward.reward_type === 'ERC20' &&
     reward.amount_per_user &&
     reward.decimals
   ) {
-    writeContract({
-      address: depositContractAddress,
-      abi: questRewardAbi,
-      functionName: 'withdrawERC20',
-      args: [
-        BigInt(questId),
-        reward.contract_address,
-        BigInt(
-          getAmount(
-            reward.amount_per_user.toString(),
-            reward.decimals
-          ).toString()
-        ),
-        BigInt(sig.nonce),
-        BigInt(sig.expiration),
-        sig.signature
-      ]
-    })
+    writeContract(
+      {
+        address: depositContractAddress,
+        abi: questRewardAbi,
+        functionName: 'withdrawERC20',
+        args: [
+          BigInt(questId),
+          reward.contract_address,
+          BigInt(
+            getAmount(
+              reward.amount_per_user.toString(),
+              reward.decimals
+            ).toString()
+          ),
+          BigInt(sig.nonce),
+          BigInt(sig.expiration),
+          sig.signature
+        ]
+      },
+      {
+        onError: logError
+      }
+    )
   } else if (isERC1155Reward && reward.decimals !== null) {
     const { token_id, amount_per_user } = reward.token_ids[0]
-    writeContract({
-      address: depositContractAddress,
-      abi: questRewardAbi,
-      functionName: 'withdrawERC1155',
-      args: [
-        BigInt(questId),
-        reward.contract_address,
-        BigInt(token_id),
-        BigInt(getAmount(amount_per_user, reward.decimals).toString()),
-        BigInt(sig.nonce),
-        BigInt(sig.expiration),
-        sig.signature
-      ]
-    })
+    writeContract(
+      {
+        address: depositContractAddress,
+        abi: questRewardAbi,
+        functionName: 'withdrawERC1155',
+        args: [
+          BigInt(questId),
+          reward.contract_address,
+          BigInt(token_id),
+          BigInt(getAmount(amount_per_user, reward.decimals).toString()),
+          BigInt(sig.nonce),
+          BigInt(sig.expiration),
+          sig.signature
+        ]
+      },
+      {
+        onError: logError
+      }
+    )
   } else if (reward.reward_type === 'ERC721' && reward.amount_per_user) {
-    writeContract({
-      address: depositContractAddress,
-      abi: questRewardAbi,
-      functionName: 'withdrawERC721',
-      args: [
-        BigInt(questId),
-        reward.contract_address,
-        // TODO: supply token id from return statement of get sig
-        BigInt('0'),
-        BigInt(sig.nonce),
-        BigInt(sig.expiration),
-        sig.signature
-      ]
-    })
+    writeContract(
+      {
+        address: depositContractAddress,
+        abi: questRewardAbi,
+        functionName: 'withdrawERC721',
+        args: [
+          BigInt(questId),
+          reward.contract_address,
+          // TODO: supply token id from return statement of get sig
+          BigInt('0'),
+          BigInt(sig.nonce),
+          BigInt(sig.expiration),
+          sig.signature
+        ]
+      },
+      {
+        onError: logError
+      }
+    )
   }
 }
