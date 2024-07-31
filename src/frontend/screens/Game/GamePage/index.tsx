3d8da4a6ce8d20d984cc9cd9cc70cba329533ee0
Flavio F Lima <26871415+flavioislima@users.noreply.github.com>
2024-07-31 17:10:40 +0100

    fix: lint

--- src/frontend/screens/Game/GamePage/index.tsx
+++ src/frontend/screens/Game/GamePage/index.tsx
@@ -551,7 +551,11 @@ export default observer(function GamePage(): JSX.Element | null {
                       )}
                     </>
                   )}
-                  <TimeContainer runner={runner} game={appName} status={status} />
+                  <TimeContainer
+                    runner={runner}
+                    game={appName}
+                    status={status}
+                  />
                 </div>
               </div>
               <div className="gameStatus">
