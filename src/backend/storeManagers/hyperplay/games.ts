0f164b3054016b579cc036662fdebe1964f16849
Free Spirit <zach@valist.io>
2024-08-07 16:01:11 -0400

    [Tech] Remove references to valistBaseApiUrlv1 (#989)
    * Remove references to valistBaseApiUrlv1
    
    * Make pretty
    
    * improve error msg
    
    ---------
    
    Co-authored-by: Brett <27568879+BrettCleary@users.noreply.github.com>

--- src/backend/storeManagers/hyperplay/games.ts
+++ src/backend/storeManagers/hyperplay/games.ts
@@ -74,7 +74,7 @@ import Store from 'electron-store'
 import i18next from 'i18next'
 import { gameManagerMap } from '..'
 import { runWineCommand } from 'backend/launcher'
-import { DEV_PORTAL_URL, valistBaseApiUrlv1 } from 'common/constants'
+import { DEV_PORTAL_URL } from 'common/constants'
 import getPartitionCookies from 'backend/utils/get_partition_cookies'
 
 interface ProgressDownloadingItem {
@@ -659,11 +659,15 @@ async function getTokenGatedPlatforms(
     address,
     channel_id
   }
-  const validateUrl = `${valistBaseApiUrlv1}/license_contracts/validate`
+  const validateUrl = `${DEV_PORTAL_URL}api/v1/license_contracts/validate`
   const validateResponse = await fetch(validateUrl, {
     method: 'POST',
     body: JSON.stringify(request)
   })
+
+  if (!validateResponse.ok) {
+    throw `Could not validate access ${await validateResponse.text()}`
+  }
 
   const validateResult: LicenseConfigValidateResult =
     await validateResponse.json()
