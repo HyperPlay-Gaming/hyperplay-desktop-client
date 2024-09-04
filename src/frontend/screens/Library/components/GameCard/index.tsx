fdd384c6d0bf0fc4da84ba6551cf15814f3b561c
Flávio Fearn <26871415+flavioislima@users.noreply.github.com>
2024-09-04 23:12:31 +0100

    [FIX] Don't show Game Settings for Browser games on game card (#1054)
    Co-authored-by: Flavio F Lima <flavioislima@users.noreply.github.com>
    Co-authored-by: Brett <27568879+BrettCleary@users.noreply.github.com>

--- src/frontend/screens/Library/components/GameCard/index.tsx
+++ src/frontend/screens/Library/components/GameCard/index.tsx
@@ -165,7 +165,8 @@ const GameCard = ({
   }
 
   const isHiddenGame = libraryState.isGameHidden(appName)
-  const isBrowserGame = installPlatform === 'Browser'
+  const isBrowserGame =
+    installPlatform === 'Browser' || installPlatform === 'web'
 
   const onUninstallClick = function () {
     setShowUninstallModal(true)
