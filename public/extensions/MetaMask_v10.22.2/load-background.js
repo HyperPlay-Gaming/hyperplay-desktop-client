f0675d15cc374730466864fd979724d29d24590b
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-09-23 19:51:20 -0700

    update mm to 12.0.5

--- public/extensions/MetaMask_v10.22.2/load-background.js
+++ public/extensions/MetaMask_v10.22.2/load-background.js
@@ -4,7 +4,7 @@
 setTimeout(() => {
   // eslint-disable-next-line spaced-comment
   const scriptsToLoad = [
-    ...["./scripts/snow.js","./scripts/use-snow.js","./scripts/sentry-install.js","./scripts/runtime-lavamoat.js","./scripts/lockdown-more.js","./scripts/policy-load.js","./common-0.js","./common-1.js","./common-2.js","./common-3.js","./common-4.js","./common-5.js","./common-6.js","./common-7.js","./common-8.js","./background-0.js","./background-1.js","./background-2.js","./background-3.js","./background-4.js","./background-5.js","./background-6.js"]
+    ...["./scripts/snow.js","./scripts/use-snow.js","./scripts/sentry-install.js","./scripts/runtime-lavamoat.js","./scripts/lockdown-more.js","./scripts/policy-load.js","./common-0.js","./common-1.js","./common-2.js","./common-3.js","./common-4.js","./common-5.js","./common-6.js","./common-7.js","./common-8.js","./common-9.js","./background-0.js","./background-1.js","./background-2.js","./background-3.js","./background-4.js","./background-5.js","./background-6.js"]
   ];
 
   const loadScript = (src) => {
