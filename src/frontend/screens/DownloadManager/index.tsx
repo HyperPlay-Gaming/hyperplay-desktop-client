5aa423cc46c24fc559e3cd468fff0efe4395baf5
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-09-24 10:32:59 +0100

    chore: add labels for empty queue and finished

--- src/frontend/screens/DownloadManager/index.tsx
+++ src/frontend/screens/DownloadManager/index.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { DMQueueElement, DownloadManagerState } from 'common/types'
-import { UpdateComponent } from 'frontend/components/UI'
 import ProgressHeader from './components/ProgressHeader'
 import { DMQueue } from 'frontend/types'
 import { Background, Tabs, getTabsClassNames } from '@hyperplay/ui'
@@ -10,7 +9,6 @@ import { DownloadTable } from './components/DownloadTable'
 
 export default React.memo(function DownloadManager(): JSX.Element | null {
   const { t } = useTranslation()
-  const [refreshing, setRefreshing] = useState(false)
   const [state, setState] = useState<DownloadManagerState>('idle')
   const [queuedElements, setQueuedElements] = useState<DMQueueElement[]>([])
   const [currentElement, setCurrentElement] = useState<DMQueueElement>()
@@ -19,11 +17,9 @@ export default React.memo(function DownloadManager(): JSX.Element | null {
   const appName = currentElement?.params?.appName ?? ''
 
   useEffect(() => {
-    setRefreshing(true)
     window.api.getDMQueueInformation().then(({ elements, state }: DMQueue) => {
       setCurrentElement(elements[0])
       setQueuedElements([...elements.slice(1)])
-      setRefreshing(false)
       setState(state)
     })
 
@@ -56,10 +52,6 @@ export default React.memo(function DownloadManager(): JSX.Element | null {
   useEffect(() => {
     window.api.trackScreen('Download Manager')
   }, [])
-
-  if (refreshing) {
-    return <UpdateComponent />
-  }
 
   const doneElements =
     (finishedElem?.length &&
@@ -117,12 +109,22 @@ export default React.memo(function DownloadManager(): JSX.Element | null {
           <Tabs.Panel value="queued">
             <div className={styles.dmItemList}>
               <DownloadTable elements={queuedElements} time="queued" />
+              {queuedElements.length === 0 && (
+                <h5 className={styles.empty}>
+                  {t('queue.label.empty.queue', 'Nothing to download')}
+                </h5>
+              )}
             </div>
           </Tabs.Panel>
           <Tabs.Panel value="downloaded">
             <div className={styles.downloadManager}>
               <div className={styles.downloadList}>
                 <DownloadTable elements={doneElements} time="finished" />
+                {doneElements.length === 0 && (
+                  <h5 className={styles.empty}>
+                    {t('queue.label.empty.finished', 'Nothing downloaded')}
+                  </h5>
+                )}
               </div>
             </div>
           </Tabs.Panel>
