e0facc67cf3668d933a6a6eeee091edb7977bc39
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-10-01 12:08:31 +0100

    fix: lint

--- src/frontend/hooks/hasProgress.ts
+++ src/frontend/hooks/hasProgress.ts
@@ -70,7 +70,8 @@ export const hasProgress = (appName: string, isExtracting?: boolean) => {
   if (!currentApp) {
     return {
       progress: nullProgress,
-      previousProgress
+      previousProgress,
+      etaInMs: 0
     }
   }
 
