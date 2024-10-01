b8da6b5bb8a73ebb5171b6e3dd3ee47b69c9b786
jiyuu-jin <zach@valist.io>
2024-10-01 18:03:44 -0400

    lint

--- src/backend/storeManagers/hyperplay/games.ts
+++ src/backend/storeManagers/hyperplay/games.ts
@@ -781,7 +781,8 @@ export async function cancelExtraction(appName: string) {
     }
   } catch (error: unknown) {
     logInfo(
-      `cancelExtraction: Error while canceling the operation ${(error as Error).message
+      `cancelExtraction: Error while canceling the operation ${
+        (error as Error).message
       } `,
       LogPrefix.HyperPlay
     )
