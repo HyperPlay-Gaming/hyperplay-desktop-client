d40b5e54b5c07576c12bb67b84380288983952a2
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-10-07 22:20:23 -0700

    whitelist app.game7.io wrt webview controls

--- src/frontend/components/UI/WebviewControls/index.tsx
+++ src/frontend/components/UI/WebviewControls/index.tsx
@@ -90,7 +90,11 @@ export default function WebviewControls({
   )
 
   const _url = url !== '' ? new URL(url) : null
-  const allowList = ['store.hyperplay.xyz', 'docs.hyperplay.xyz']
+  const allowList = [
+    'store.hyperplay.xyz',
+    'docs.hyperplay.xyz',
+    'app.game7.io'
+  ]
 
   if (_url && allowList.includes(_url.host)) return null
 
