62cdd24c6a5646d469fee39e5f9639f65f1c9101
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-10-17 01:40:55 -0700

    fix loader

--- src/frontend/screens/MetaMaskPortfolio/index.tsx
+++ src/frontend/screens/MetaMaskPortfolio/index.tsx
@@ -1,37 +1,18 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import MetaMaskPortfolioStyles from './index.module.scss'
 import { useParams } from 'react-router-dom'
-import classNames from 'classnames'
 
 const MetaMaskPortfolio = function () {
   const trueAsStr = 'true' as unknown as boolean | undefined
   const { page = '' } = useParams() as { page: string }
-  const [loaded, setLoaded] = useState(false)
   const webviewRef = useRef<HTMLWebViewElement>(null)
-
-  let content = null
-  if (!loaded) {
-    content = <div className={MetaMaskPortfolioStyles.loader}></div>
-  }
-
-  const webviewClassnames: Record<string, boolean> = {}
-  webviewClassnames[MetaMaskPortfolioStyles.hide] = !loaded
-
-  useEffect(() => {
-    webviewRef.current?.addEventListener('did-finish-load', () => {
-      setLoaded(true)
-    })
-  }, [])
 
   return (
     <>
       <div className={MetaMaskPortfolioStyles.homeDiv}>
-        {content}
+        <div className={MetaMaskPortfolioStyles.loader}></div>
         <webview
-          className={classNames(
-            MetaMaskPortfolioStyles.homeWebview,
-            webviewClassnames
-          )}
+          className={MetaMaskPortfolioStyles.homeWebview}
           src={`https://portfolio.metamask.io/${page}`}
           allowpopups={trueAsStr}
           partition={'persist:InPageWindowEthereumExternalWallet'}
