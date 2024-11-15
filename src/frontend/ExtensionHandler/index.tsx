543463a3eda7c0be5fc3604b712b7cdb5e3b64d7
Brett <27568879+BrettCleary@users.noreply.github.com>
2024-11-15 15:21:54 -0800

    [Fix] ext lock connect (#1149)
    * fix mm ext unlock after webview launch
    
    * run yarn setup
    
    * rm unused imports
    
    * improve window.ethereum typing across the app

--- src/frontend/ExtensionHandler/index.tsx
+++ src/frontend/ExtensionHandler/index.tsx
@@ -3,22 +3,6 @@ import { observer } from 'mobx-react-lite'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-declare global {
-  interface Window {
-    ethereum: {
-      /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
-      request: (args: any) => any
-      /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
-      send: (...args: any) => any
-      /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
-      sendAsync: (...args: any) => any
-      /*eslint-disable-next-line @typescript-eslint/no-explicit-any */
-      on: (topic: string, handler: (...args: any) => void) => void
-      isConnected: () => boolean
-    }
-  }
-}
-
 const ExtensionHandler = observer(function () {
   const navigate = useNavigate()
 
