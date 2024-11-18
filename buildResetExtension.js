853e5c78c50bbfd9dfe9d5c43510de745d264b3d
Flavio F Lima <flavioislima@users.noreply.github.com>
2024-11-18 19:36:33 +0000

    tech: update docs and rest of files

--- buildResetExtension.js
+++ buildResetExtension.js
@@ -9,7 +9,7 @@ const resetExtensionScript =
   './node_modules/@hyperplay/extension-importer/src/resetExtension.js'
 if (existsSync(resetExtensionScript)) {
   console.log('spawning pkg', resetExtensionScript, args)
-  const command = platform() === 'win32' ? 'yarn.cmd' : 'yarn'
+  const command = platform() === 'win32' ? 'pnpm.cmd' : 'pnpm'
   const result = spawnSync(
     command,
     [
