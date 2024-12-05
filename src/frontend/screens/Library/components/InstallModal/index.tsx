f503d8e64d0730772bd31e421d3c6a1ee3679b86
Elio Briceño <eliobricenov@gmail.com>
2024-12-05 16:56:29 -0400

    feat(install-modal): block access code usage until user is logged in

--- src/frontend/screens/Library/components/InstallModal/index.tsx
+++ src/frontend/screens/Library/components/InstallModal/index.tsx
@@ -29,6 +29,8 @@ import ChannelNameSelection from 'frontend/components/UI/ChannelNameSelection'
 import gameRequiresAccessCodes from 'frontend/helpers/gameRequiresAccessCodes'
 import ModDialog from './ModDialog'
 import { AccessCodeInput } from 'frontend/components/UI/AccessCodeInput'
+import useAuthSession from 'frontend/hooks/useAuthSession'
+import { AlertCard } from '@hyperplay/ui'
 
 type Props = {
   appName: string
@@ -44,6 +46,7 @@ export default React.memo(function InstallModal({
   gameInfo = null
 }: Props) {
   const { platform } = useContext(ContextProvider)
+  const { isSignedIn } = useAuthSession()
 
   const [winePrefix, setWinePrefix] = useState('...')
   const [wineVersion, setWineVersion] = useState<WineInstallation>()
@@ -198,6 +201,23 @@ export default React.memo(function InstallModal({
     />
   )
 
+  let accessCodeContent = null
+
+  if (channelRequiresAccessCode && runner === 'hyperplay') {
+    if (!isSignedIn) {
+      accessCodeContent = (
+        <AlertCard
+          showClose={false}
+          title="Login Required"
+          message="You need to be logged into HyperPlay to enter your access code and install this game."
+          variant="warning"
+        />
+      )
+    } else {
+      accessCodeContent = accessCodeInput
+    }
+  }
+
   return (
     <div className="InstallModal">
       <Dialog
@@ -236,10 +256,7 @@ export default React.memo(function InstallModal({
                 gameInfo={gameInfo}
               />
             ) : null}
-            {runner === 'hyperplay' && channelRequiresAccessCode
-              ? accessCodeInput
-              : null}
-
+            {accessCodeContent}
             {hasWine ? (
               <WineSelector
                 winePrefix={winePrefix}
