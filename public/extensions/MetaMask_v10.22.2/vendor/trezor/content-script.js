ed402296b18bbad31204e53faa3d4a5288675fdc
Brett <27568879+BrettCleary@users.noreply.github.com>
2025-01-28 11:51:48 -0800

    fix ui only

--- public/extensions/MetaMask_v10.22.2/vendor/trezor/content-script.js
+++ public/extensions/MetaMask_v10.22.2/vendor/trezor/content-script.js
@@ -1,27 +1,14 @@
 /*
 Passing messages from background script to popup
 */
-let port;
 
-function init(){
-    port = chrome.runtime.connect({ name: 'trezor-connect' });
-    port.onMessage.addListener(message => {
-        window.postMessage(message, window.location.origin);
-    });
-    port.onDisconnect.addListener(d => {
-        port = null;
-    });
-}
-
-init();
-
-navigator.serviceWorker.oncontrollerchange = () => {
-  console.log(
-    "This content script is now controlled by",
-    navigator.serviceWorker.controller,
-  );
-  init();
-};
+let port = chrome.runtime.connect({ name: 'trezor-connect' });
+port.onMessage.addListener(message => {
+    window.postMessage(message, window.location.origin);
+});
+port.onDisconnect.addListener(d => {
+    port = null;
+});
 
 /*
 Passing messages from popup to background script
