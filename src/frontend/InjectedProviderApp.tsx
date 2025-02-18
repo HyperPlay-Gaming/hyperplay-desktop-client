feba72cd77c8bd61bf120c1140bc2dbe9f797ed1
Brett <27568879+BrettCleary@users.noreply.github.com>
2025-02-17 17:09:46 -0800

    add back log

--- src/frontend/InjectedProviderApp.tsx
+++ src/frontend/InjectedProviderApp.tsx
@@ -3,5 +3,10 @@ import InjectedProviderHandler from './state/InjectedProviderHandler'
 import { observer } from 'mobx-react-lite'
 
 export default observer(function () {
+  // @dev need to keep this log so that InjectedProviderHandler is imported and initialized
+  console.log(
+    'eth listeners bound: ',
+    InjectedProviderHandler.ethereumListenersBound
+  )
   return <div></div>
 })
