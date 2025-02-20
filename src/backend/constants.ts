fe31dc9b8546847250bb86e9248e8f8460e67f16
Elio Briceño <eliobricenov@gmail.com>
2025-02-20 13:27:17 +0100

    chore: use prod dev portal url

--- src/backend/constants.ts
+++ src/backend/constants.ts
@@ -107,8 +107,7 @@ const eventsToCloseMetaMaskPopupOn = [
   'mouseDown',
   'mouseUp'
 ]
-const valistListingsApiUrl =
-  'https://gameplay-wallet-preview.hyperplay.xyz/api/v1/listings'
+const valistListingsApiUrl = 'https://developers.hyperplay.xyz/api/v1/listings'
 const mainReleaseChannelName = 'main'
 export let qaToken = ''
 
