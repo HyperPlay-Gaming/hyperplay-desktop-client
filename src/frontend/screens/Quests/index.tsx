2e6da1ae41e4194c388796702ed571033d5fa6e7
Brett <27568879+BrettCleary@users.noreply.github.com>
2025-02-25 23:14:49 -0800

    rm quest table sort, get quests by start date newest first

--- src/frontend/screens/Quests/index.tsx
+++ src/frontend/screens/Quests/index.tsx
@@ -41,10 +41,6 @@ export function QuestsPage() {
   const searchParam = searchParams.get('search')
   const [searchText, setSearchText] = useState(searchParam ?? '')
   const [activeFilter, setActiveFilter] = useState<QuestFilter>('all')
-  const [selectedSort, setSelectedSort] = useState<itemType>({
-    text: 'Alphabetically (ASC)',
-    id: 'ALPHA_ASC'
-  })
 
   useEffect(() => {
     window.api.trackScreen('Quests Page')
@@ -123,17 +119,12 @@ export function QuestsPage() {
     }
   })
 
-  const sortedQuests = [...(quests ?? [])].sort((a, b) => {
-    const sortMultiplier = selectedSort.id === 'ALPHA_ASC' ? 1 : -1
-    return a.name.localeCompare(b.name) * sortMultiplier
-  })
-
   const gameTitleMatches = (quest: Quest) => {
     const title = listings ? listings[quest.project_id]?.project_meta?.name : ''
     return title?.toLowerCase().startsWith(searchText.toLowerCase())
   }
 
-  const searchFilteredQuests = sortedQuests?.filter((quest) => {
+  const searchFilteredQuests = quests?.filter((quest) => {
     const questTitleMatch = quest.name
       .toLowerCase()
       .startsWith(searchText.toLowerCase())
@@ -151,11 +142,6 @@ export function QuestsPage() {
 
   const initialQuestId = searchFilteredQuests?.[0]?.id ?? null
   const visibleQuestId = selectedQuestId ?? initialQuestId
-
-  const achievementsSortOptions = [
-    { text: 'Alphabetically (ASC)', id: 'ALPHA_ASC' },
-    { text: 'Alphabetically (DES)', id: 'ALPHA_DES' }
-  ]
 
   const imagesToPreload: string[] = []
   const gameElements =
@@ -206,11 +192,6 @@ export function QuestsPage() {
         <QuestsSummaryTable
           games={gameElements}
           imagesToPreload={imagesToPreload}
-          sortProps={{
-            options: achievementsSortOptions,
-            selected: selectedSort,
-            onItemChange: setSelectedSort
-          }}
           filterProps={{ activeFilter, setActiveFilter }}
           isFetching={questsResults?.data.isFetching}
           isPageLoading={questsResults?.data.isLoading}
