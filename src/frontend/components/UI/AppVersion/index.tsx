3910d7321260a092d95d329333fe75df71870bca
Flavio F Lima <flavioislima@users.noreply.github.com>
2025-03-05 11:31:51 +0000

    feat: reload app if a new version is detected

--- src/frontend/components/UI/AppVersion/index.tsx
+++ src/frontend/components/UI/AppVersion/index.tsx
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from 'react'
 
 const storage = window.localStorage
-let lastVersion = storage.getItem('last_version')?.replaceAll('"', '')
-if (lastVersion === undefined) {
-  lastVersion = ''
-}
+const lastVersion = storage.getItem('last_version')?.replaceAll('"', '')
 
 export default function AppVersion() {
   const [appVersion, setAppVersion] = useState(lastVersion)
 
   useEffect(() => {
     window.api.getAppVersion().then((version) => {
+      if (version !== lastVersion) {
+        window.api.logInfo(
+          `Updated to a new version, reloading the MetaMask extension. Old version: ${lastVersion}, new version: ${version}`
+        )
+        window.api.reloadForMM()
+      } else {
+        window.api.logInfo('No new version detected. Wont reload the app')
+      }
       storage.setItem('last_version', JSON.stringify(version))
       setAppVersion(version)
     })
