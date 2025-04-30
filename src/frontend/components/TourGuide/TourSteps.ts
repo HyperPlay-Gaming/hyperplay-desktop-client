0dd6547d60dd19bf82d5ccc444a5e605055a6335
Flavio F Lima <flavioislima@users.noreply.github.com>
2025-04-30 18:48:33 +0100

    fix: capitalization

--- src/frontend/components/TourGuide/TourSteps.ts
+++ src/frontend/components/TourGuide/TourSteps.ts
@@ -12,7 +12,7 @@ export interface TourStep {
 // First Welcome tour steps for new users landing on the Store Page
 export const firstWelcomeTourSteps = (t: TFunction<'tour'>): TourStep[] => [
   {
-    title: t('tour.first-welcome.title1', 'Ready for takeoff 🚀'),
+    title: t('tour.first-welcome.welcome-title', 'Ready for Takeoff 🚀'),
     intro: t(
       'tour.first-welcome.welcome',
       'Begin your journey through the Web3 galaxy and discover top-tier games.'
@@ -21,7 +21,7 @@ export const firstWelcomeTourSteps = (t: TFunction<'tour'>): TourStep[] => [
   },
   {
     element: '[data-tour="topnav-store-links"]',
-    title: t('tour.first-welcome.title2', 'A galaxy of games'),
+    title: t('tour.first-welcome.links-title', 'A Galaxy of Games'),
     intro: t(
       'tour.first-welcome.stores',
       'Explore all the titles from HyperPlay, Epic Games, and GOG.'
@@ -30,7 +30,7 @@ export const firstWelcomeTourSteps = (t: TFunction<'tour'>): TourStep[] => [
   },
   {
     element: '[data-tour="sidebar-quests"]',
-    title: t('tour.first-welcome.title3', 'Earn real rewards 🎁'),
+    title: t('tour.first-welcome.rewards-title', 'Earn Real Rewards 🎁'),
     intro: t(
       'tour.first-welcome.quests',
       'Take on quests, make leaderboards, and earn along the way.'
@@ -39,7 +39,7 @@ export const firstWelcomeTourSteps = (t: TFunction<'tour'>): TourStep[] => [
   },
   {
     element: '[data-tour="topnav-account"]',
-    title: t('tour.first-welcome.title4', 'Start your journey'),
+    title: t('tour.first-welcome.account-title', 'Start Your Journey'),
     intro: t(
       'tour.first-welcome.account',
       'Login and connect your primary wallet to track progress.'
