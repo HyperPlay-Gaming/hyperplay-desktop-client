83a5b8ee568a560b94aa4386d9581cfaa6007544
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-05-27 23:13:21 +0100

    Refactor SideloadDialogWarning to fix bug and improve UI

--- src/frontend/screens/Library/components/InstallModal/SideloadDialog/Warning/index.tsx
+++ src/frontend/screens/Library/components/InstallModal/SideloadDialog/Warning/index.tsx
@@ -1,11 +1,8 @@
 import React, { useState } from 'react'
-import { AlertCard, Typography, Images } from '@hyperplay/ui'
-
+import { AlertCard, Images, Typography } from '@hyperplay/ui'
+import { Link } from 'react-router-dom'
 import styles from './index.module.scss'
 import { t } from 'i18next'
-
-const { Button } = Typography
-const { WarningIcon } = Images
 
 export default function SideloadDialogWarning() {
   const [isOpen, setIsOpen] = useState(true)
@@ -13,27 +10,30 @@ export default function SideloadDialogWarning() {
   return (
     <div className={styles.container}>
       {isOpen ? (
+        <a className={styles.warningAlert}>
+          <Images.AlertOctagon className={styles.warningAlertIcon} />
+          <Typography.Button>
+            <Link
+              to="#"
+              className={styles.warningAlertLink}
+              onClick={() => setIsOpen(!isOpen)}
+            >
+              {t('sideload.warningMessageLink', 'Important')}
+            </Link>
+          </Typography.Button>
+        </a>
+      ) : (
         <AlertCard
-          icon={<Images.WarningIcon />}
+          icon={<Images.AlertOctagon />}
           style={{ maxWidth: 670 }}
           onClose={() => setIsOpen(!isOpen)}
-          variant="warning"
+          variant="error"
           title={t('sideload.warningTitle', 'Important')}
           message={t(
             'sideload.warningMessage',
             'Side-loading a game is a feature intended for developers and experienced power users. Side-loading an installer that contains malware could compromise your computer and lead to theft of funds.'
           )}
         />
-      ) : (
-        <div
-          className={styles.titleContainer}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          <WarningIcon />
-          <Button className={styles.title}>
-            {t('sideload.warningTitle', 'Important')}
-          </Button>
-        </div>
       )}
     </div>
   )
