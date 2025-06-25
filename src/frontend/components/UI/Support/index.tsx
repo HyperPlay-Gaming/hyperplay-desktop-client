cab23da97c482496d855f9884768d02d9fa1b856
BrettCleary <27568879+BrettCleary@users.noreply.github.com>
2025-06-25 00:21:40 -0700

    update button show logic, add unread message counter

--- src/frontend/components/UI/Support/index.tsx
+++ src/frontend/components/UI/Support/index.tsx
@@ -1,6 +1,7 @@
 import { Button, Images } from '@hyperplay/ui'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './index.module.scss'
+import classNames from 'classnames'
 
 declare global {
   interface Window {
@@ -10,19 +11,38 @@ declare global {
 }
 
 export const Support = () => {
+  const [count, setCount] = useState(0)
+  const [modalIsShown, setModalIsShown] = useState(false)
   useEffect(() => {
-    window.zE?.('messenger:on', 'unreadMessages', (count: number) => {
-      console.log('count ', count)
+    window.zE?.('messenger:on', 'unreadMessages', (newCount: number) => {
+      setCount(newCount)
     })
-  })
+    window.zE?.('messenger:on', 'open', () => {
+      setModalIsShown(true)
+    })
+    window.zE?.('messenger:on', 'close', () => {
+      setModalIsShown(false)
+    })
+  }, [])
   return (
     <>
       <Button
         type="tertiary-neutral"
         size="icon"
-        onClick={() => window.zE('messenger', 'open')}
+        onClick={() => {
+          if (modalIsShown) {
+            window.zE('messenger', 'close')
+          } else {
+            window.zE('messenger', 'open')
+          }
+        }}
       >
         <Images.QuestionMarkRounded className={styles.icon} />
+        {count ? (
+          <div className={classNames('eyebrow', styles.unreadCounter)}>
+            {count}
+          </div>
+        ) : null}
       </Button>
     </>
   )
