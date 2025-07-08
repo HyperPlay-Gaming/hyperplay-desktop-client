4dc3b4a19bc7188022244630b564bee99af0ee7a
Biliane Silva <61093873+biliesilva@users.noreply.github.com>
2025-07-08 01:39:14 +0100

    feat: add new wallet connectio information with new layout and content updates

--- src/frontend/screens/Onboarding/walletSelection/screens/info/index.tsx
+++ src/frontend/screens/Onboarding/walletSelection/screens/info/index.tsx
@@ -1,23 +1,25 @@
 import React from 'react'
 
 import WalletInfoStyles from './index.module.scss'
-import WalletOptionIcon from 'frontend/assets/hyperplay/WalletOptionsIcon.png'
-import WalletIcon from 'frontend/assets/hyperplay/WalletIcon.png'
-import { Button } from '@hyperplay/ui'
+import { ConnectWallet, WarningIcon } from 'frontend/assets/hyperplay'
+
 import { t } from 'i18next'
 
+interface WalletInfoScreenProps {
+  createWalletClicked: () => void
+  mmInitialized: boolean
+}
+
 interface InfoTextProps {
-  icon: string
+  icon: React.ReactNode
   title: string
-  details: string
+  details?: string
 }
 
 const InfoText = function ({ icon, title, details }: InfoTextProps) {
   return (
     <div className={WalletInfoStyles.walletInfoContainer}>
-      <div>
-        <img src={icon} />
-      </div>
+      <div>{icon}</div>
       <div className={WalletInfoStyles.walletTextContainer}>
         <div className="menu">{title}</div>
         <div className={`caption-sm ${WalletInfoStyles.infoTextCaption}`}>
@@ -28,66 +30,84 @@ const InfoText = function ({ icon, title, details }: InfoTextProps) {
   )
 }
 
-interface WalletInfoScreenProps {
-  skipClicked: () => void
-  createWalletClicked: () => void
-  mmInitialized: boolean
-}
-
 const WalletInfoScreen = (props: WalletInfoScreenProps) => {
   return (
-    <div className={WalletInfoStyles.walletInfoScreen}>
-      <div className={`title ${WalletInfoStyles.title}`}>
-        {t(
-          'hyperplay.onboarding.walletSelection.screens.info.title',
-          `What is a wallet?`
-        )}
-      </div>
-      <InfoText
-        icon={WalletOptionIcon}
-        title={t(
-          'hyperplay.onboarding.walletSelection.screens.info.digitalAssets.title',
-          `A Home for your Digital Assets`
-        )}
-        details={t(
-          'hyperplay.onboarding.walletSelection.screens.info.digitalAssets.details',
-          `Wallets are used to send, receive, store, and display digital assets like tokens and NFTs.`
-        )}
-      />
-      <InfoText
-        icon={WalletIcon}
-        title={t(
-          'hyperplay.onboarding.walletSelection.screens.info.login.title',
-          `A New Way to Log In`
-        )}
-        details={t(
-          'hyperplay.onboarding.walletSelection.screens.info.login.details',
-          `An alternative to creating new accounts and passwords on every website, just connect your wallet instead.`
-        )}
-      />
-      <div className={`caption-sm ${WalletInfoStyles.caption}`}>
-        {t(
-          'hyperplay.onboarding.walletSelection.screens.info.createWalletDescription',
-          `Click in “Create a wallet” below if you do not have a wallet or want to create a new one:`
-        )}
-      </div>
-      <div className={WalletInfoStyles.actionButtonContainer}>
-        <Button type="tertiary" onClick={props.skipClicked}>
+    <>
+      <div className={WalletInfoStyles.walletInfoScreen}>
+        <div className={WalletInfoStyles.connectWalletSvgContainer}>
+          <span className={WalletInfoStyles.blurBackground}></span>
+          <ConnectWallet className={WalletInfoStyles.walletSvg} />
+        </div>
+        <div className={`title ${WalletInfoStyles.title}`}>
           {t(
-            'hyperplay.onboarding.walletSelection.screens.info.skip',
-            `Skip for now`
+            'hyperplay.onboarding.walletSelection.screens.newInfo.title',
+            `Don't Just Play-Get Rewarded`
           )}
-        </Button>
-        {!props.mmInitialized ? (
-          <Button onClick={props.createWalletClicked}>
+        </div>
+        <div className={`body-sm ${WalletInfoStyles.smallTitle}`}>
+          {t(
+            'hyperplay.onboarding.walletSelection.screens.info.digitalAssets.smallTitle',
+            `Connect your wallet to activate powerful features:`
+          )}
+        </div>
+        <div className={`body-sm ${WalletInfoStyles.description}`}>
+          <li>
+            {' '}
             {t(
-              'hyperplay.onboarding.walletSelection.screens.info.createWalletCTA',
-              `Create a wallet`
+              'hyperplay.onboarding.walletSelection.screens.info.list.bullet1',
+              `Earn and claim quest rewards`
             )}
-          </Button>
-        ) : null}
+            <li>
+              {t(
+                'hyperplay.onboarding.walletSelection.screens.info.list.bullet2',
+                `Track your in-game progress`
+              )}
+            </li>
+            <li>
+              {t(
+                'hyperplay.onboarding.walletSelection.screens.info.list.bullet3',
+                `Access in-game transactions and marketplaces`
+              )}
+            </li>
+            <li>
+              {t(
+                'hyperplay.onboarding.walletSelection.screens.info.list.bullet4',
+                `Buy, swap, and send tokens`
+              )}
+            </li>
+          </li>
+        </div>
+        <div>
+          <span className={WalletInfoStyles.noWalletText}>
+            {t(
+              'hyperplay.onboarding.walletSelection.screens.info.noWallet',
+              `Don't have a wallet?`
+            )}
+          </span>
+          <a
+            rel="noreferrer"
+            href="https://metamask.io/"
+            target="_blank"
+            className={WalletInfoStyles.createWalletLink}
+            onClick={props.createWalletClicked}
+          >
+            {t(
+              'hyperplay.onboarding.walletSelection.screens.info.createWalletLink',
+              `Get MetaMask`
+            )}
+          </a>
+        </div>
+        <div className={`eyebrow-sm ${WalletInfoStyles.walletIssues}`}>
+          <InfoText
+            icon={<WarningIcon className={WalletInfoStyles.warningIcon} />}
+            title={t(
+              'hyperplay.onboarding.walletSelection.screens.metamask.issues',
+              `Having issues? Make sure MetaMask is up to date.`
+            )}
+          />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
